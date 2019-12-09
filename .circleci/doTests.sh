d627b3413c4ec3c691e16a5b1e1c49dc4b40a375
Rishabh Poddar <rishabh.poddar@gmail.com>
2019-12-09 18:55:55 +0530

    temp change

--- .circleci/doTests.sh
+++ .circleci/doTests.sh
@@ -56,16 +56,16 @@ while [ $i -lt $coreDriverLength ]; do
     fi
     coreCommercial=$(echo $coreCommercial | jq .core | tr -d '"')
 
-    coreFree=`curl -s -X GET \
-    "https://api.supertokens.io/0/core-driver-interface/dependency/core/latest?password=$SUPERTOKENS_API_KEY&planType=FREE&mode=DEV&version=$coreDriverVersion" \
-    -H 'api-version: 0'`
-    if [[ `echo $coreFree | jq .core` == "null" ]]
-    then
-        echo "fetching latest X.Y version for core given core-driver-interface X.Y version: $coreDriverVersion, planType: FREE gave response: $coreFree. Please make sure all relevant cores have been pushed."
-        git push --delete origin dev-v$version
-        exit 1
-    fi
-    coreFree=$(echo $coreFree | jq .core | tr -d '"')
+    # coreFree=`curl -s -X GET \
+    # "https://api.supertokens.io/0/core-driver-interface/dependency/core/latest?password=$SUPERTOKENS_API_KEY&planType=FREE&mode=DEV&version=$coreDriverVersion" \
+    # -H 'api-version: 0'`
+    # if [[ `echo $coreFree | jq .core` == "null" ]]
+    # then
+    #     echo "fetching latest X.Y version for core given core-driver-interface X.Y version: $coreDriverVersion, planType: FREE gave response: $coreFree. Please make sure all relevant cores have been pushed."
+    #     git push --delete origin dev-v$version
+    #     exit 1
+    # fi
+    # coreFree=$(echo $coreFree | jq .core | tr -d '"')
 
     someTestsRan=true
     ./setupAndTestWithCommercialCore.sh $coreCommercial
@@ -77,14 +77,14 @@ while [ $i -lt $coreDriverLength ]; do
     fi
     rm -rf ../../com-root
 
-    ./setupAndTestWithFreeCore.sh $coreFree
-    if [[ $? -ne 0 ]]
-    then
-        echo "test failed... exiting!"
-        git push --delete origin dev-v$version
-        exit 1
-    fi
-    rm -rf ../../com-root
+    # ./setupAndTestWithFreeCore.sh $coreFree
+    # if [[ $? -ne 0 ]]
+    # then
+    #     echo "test failed... exiting!"
+    #     git push --delete origin dev-v$version
+    #     exit 1
+    # fi
+    # rm -rf ../../com-root
 done
 
 if [[ $someTestsRan = "true" ]]
