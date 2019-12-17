712d2e68c0730207d1263330f731f7cb54c9a801
Rishabh Poddar <rishabh.poddar@gmail.com>
2019-12-17 21:55:34 +0530

    adds test cases todo

--- test/deviceDriverInfo.test.js
+++ test/deviceDriverInfo.test.js
@@ -8,8 +8,21 @@ const express = require("express");
 const request = require("supertest");
 let { HandshakeInfo } = require("../lib/build/handshakeInfo");
 
-// TODO: test with existing header params being there and that the lib appends to those and not overrides those
-
+/* TODO: 
+- test with existing header params being there and that the lib appends to those and not overrides those
+- check basic usage of session
+- check for token theft detection
+- check session verify for with / without anti-csrf present
+- check revoking session(s)
+- check manipulating session data
+- check if output headers and set cookies for create session is fine
+- check if output headers and set cookies for refresh session is fine
+- check that verification of session happens without calling the server
+- check that if signing key changes, things are still fine
+- check if input cookies are missing, an appropriate error is thrown
+- if anti-csrf is disabled, check that not having that in input to verify / refresh session is fine
+- the opposite of the above condition
+*/
 describe(`deviceDriverInfo: ${printPath("[test/deviceDriverInfo.test.js]")}`, function() {
     beforeEach(async function() {
         await killAllST();
