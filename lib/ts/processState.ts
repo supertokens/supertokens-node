0c2068723f6c39e93e8e01d4d9df82451818138e
Rishabh Poddar <rishabh.poddar@gmail.com>
2020-02-03 16:11:41 +0530

    fixes a bug

--- lib/ts/processState.ts
+++ lib/ts/processState.ts
@@ -18,7 +18,7 @@ export enum PROCESS_STATE {
 }
 
 export class ProcessState {
-    history: PROCESS_STATE[];
+    history: PROCESS_STATE[] = [];
     private static instance: ProcessState | undefined;
 
     private constructor() {}
