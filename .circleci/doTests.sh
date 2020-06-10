55dbfd4c28a61d884687d36b4f82af40e785607d
Bhumil Sarvaiya <sarvaiyabhumil@gmail.com>
2020-06-10 11:40:12 +0530

    test-cicd

--- .circleci/doTests.sh
+++ .circleci/doTests.sh
@@ -38,50 +38,50 @@ done <<< "$version"
 # 	exit 1
 # fi
 
-someTestsRan=false
-i=0
-while [ $i -lt $coreDriverLength ]; do 
-    coreDriverVersion=`echo $coreDriverArray | jq ".[$i]"`
-    coreDriverVersion=`echo $coreDriverVersion | tr -d '"'`
-    i=$((i+1))
+# someTestsRan=false
+# i=0
+# while [ $i -lt $coreDriverLength ]; do 
+#     coreDriverVersion=`echo $coreDriverArray | jq ".[$i]"`
+#     coreDriverVersion=`echo $coreDriverVersion | tr -d '"'`
+#     i=$((i+1))
     
-    coreCommercial=`curl -s -X GET \
-    "https://api.supertokens.io/0/core-driver-interface/dependency/core/latest?password=$SUPERTOKENS_API_KEY&planType=COMMERCIAL&mode=DEV&version=$coreDriverVersion" \
-    -H 'api-version: 0'`
-    if [[ `echo $coreCommercial | jq .core` == "null" ]]
-    then
-        echo "fetching latest X.Y version for core given core-driver-interface X.Y version: $coreDriverVersion, planType: COMMERCIAL gave response: $coreCommercial. Please make sure all relevant cores have been pushed."
-        exit 1
-    fi
-    coreCommercial=$(echo $coreCommercial | jq .core | tr -d '"')
+#     coreCommercial=`curl -s -X GET \
+#     "https://api.supertokens.io/0/core-driver-interface/dependency/core/latest?password=$SUPERTOKENS_API_KEY&planType=COMMERCIAL&mode=DEV&version=$coreDriverVersion" \
+#     -H 'api-version: 0'`
+#     if [[ `echo $coreCommercial | jq .core` == "null" ]]
+#     then
+#         echo "fetching latest X.Y version for core given core-driver-interface X.Y version: $coreDriverVersion, planType: COMMERCIAL gave response: $coreCommercial. Please make sure all relevant cores have been pushed."
+#         exit 1
+#     fi
+#     coreCommercial=$(echo $coreCommercial | jq .core | tr -d '"')
 
-    coreFree=`curl -s -X GET \
-    "https://api.supertokens.io/0/core-driver-interface/dependency/core/latest?password=$SUPERTOKENS_API_KEY&planType=FREE&mode=DEV&version=$coreDriverVersion" \
-    -H 'api-version: 0'`
-    if [[ `echo $coreFree | jq .core` == "null" ]]
-    then
-        echo "fetching latest X.Y version for core given core-driver-interface X.Y version: $coreDriverVersion, planType: FREE gave response: $coreFree. Please make sure all relevant cores have been pushed."
-        exit 1
-    fi
-    coreFree=$(echo $coreFree | jq .core | tr -d '"')
+#     coreFree=`curl -s -X GET \
+#     "https://api.supertokens.io/0/core-driver-interface/dependency/core/latest?password=$SUPERTOKENS_API_KEY&planType=FREE&mode=DEV&version=$coreDriverVersion" \
+#     -H 'api-version: 0'`
+#     if [[ `echo $coreFree | jq .core` == "null" ]]
+#     then
+#         echo "fetching latest X.Y version for core given core-driver-interface X.Y version: $coreDriverVersion, planType: FREE gave response: $coreFree. Please make sure all relevant cores have been pushed."
+#         exit 1
+#     fi
+#     coreFree=$(echo $coreFree | jq .core | tr -d '"')
 
-    someTestsRan=true
-    ./setupAndTestWithCommercialCore.sh $coreCommercial
-    if [[ $? -ne 0 ]]
-    then
-        echo "test failed... exiting!"
-        exit 1
-    fi
-    rm -rf ../../com-root
+#     someTestsRan=true
+#     ./setupAndTestWithCommercialCore.sh $coreCommercial
+#     if [[ $? -ne 0 ]]
+#     then
+#         echo "test failed... exiting!"
+#         exit 1
+#     fi
+#     rm -rf ../../com-root
 
-    ./setupAndTestWithFreeCore.sh $coreFree
-    if [[ $? -ne 0 ]]
-    then
-        echo "test failed... exiting!"
-        exit 1
-    fi
-    rm -rf ../../supertokens-root
-done
+#     ./setupAndTestWithFreeCore.sh $coreFree
+#     if [[ $? -ne 0 ]]
+#     then
+#         echo "test failed... exiting!"
+#         exit 1
+#     fi
+#     rm -rf ../../supertokens-root
+# done
 
 someFrontendTestsRan=false
 i=0
@@ -148,25 +148,25 @@ while [ $i -lt $frontendDriverLength ]; do
     rm -rf ../../com-root
 done
 
-if [[ $someFrontendTestsRan = "true" ]] && [[ $someTestsRan = "true" ]]
-then
-    echo "calling /driver PATCH to make testing passed"
-    responseStatus=`curl -s -o /dev/null -w "%{http_code}" -X PATCH \
-        https://api.supertokens.io/0/driver \
-        -H 'Content-Type: application/json' \
-        -H 'api-version: 0' \
-        -d "{
-            \"password\": \"$SUPERTOKENS_API_KEY\",
-            \"version\":\"$version\",
-            \"name\": \"node\",
-            \"testPassed\": true
-        }"`
-    if [ $responseStatus -ne "200" ]
-    then
-        echo "patch api failed"
-        exit 1
-    fi
-else
-    echo "no test ran"
-    exit 1
-fi
+# if [[ $someFrontendTestsRan = "true" ]] && [[ $someTestsRan = "true" ]]
+# then
+#     echo "calling /driver PATCH to make testing passed"
+#     responseStatus=`curl -s -o /dev/null -w "%{http_code}" -X PATCH \
+#         https://api.supertokens.io/0/driver \
+#         -H 'Content-Type: application/json' \
+#         -H 'api-version: 0' \
+#         -d "{
+#             \"password\": \"$SUPERTOKENS_API_KEY\",
+#             \"version\":\"$version\",
+#             \"name\": \"node\",
+#             \"testPassed\": true
+#         }"`
+#     if [ $responseStatus -ne "200" ]
+#     then
+#         echo "patch api failed"
+#         exit 1
+#     fi
+# else
+#     echo "no test ran"
+#     exit 1
+# fi
