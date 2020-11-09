201e3f6ade4dae5b2d3c59624343209fdeaca195
Joel Coutinho <joelcoutinho@qually.io>
2020-11-09 14:42:22 +0530

    run pretty

--- test/emailpassword/config.test.js
+++ test/emailpassword/config.test.js
@@ -233,6 +233,7 @@ describe(`configTest: ${printPath("[test/config.test.js]")}`, function () {
         assert((await defaultEmailValidator("aaaaaa@aaaaaa")) === "Email is invalid");
         assert((await defaultEmailValidator("random  User   @randomMail.com")) === "Email is invalid");
         assert((await defaultEmailValidator("*@*")) === "Email is invalid");
+        assert((await defaultEmailValidator("validmail@gmail.com")) === undefined);
         await defaultEmailValidator();
 
         let defaultPasswordValidator = formFields.filter((f) => f.id === "password")[0].validate;
@@ -242,6 +243,7 @@ describe(`configTest: ${printPath("[test/config.test.js]")}`, function () {
         );
         assert((await defaultPasswordValidator("aaaaaaaaa")) === "Password must contain at least one number");
         assert((await defaultPasswordValidator("1234*-56*789")) === "Password must contain at least one alphabet");
+        assert((await defaultPasswordValidator("validPass123")) === undefined);
         await defaultPasswordValidator();
     });
 });
