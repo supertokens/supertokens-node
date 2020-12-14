ad2d3aaef494b0e8633431086593a10245e9f865
NkxxkN <kevin.antoine@protonmail.com>
2020-12-14 16:08:15 +0100

    update version to 3.1.2

--- lib/ts/version.ts
+++ lib/ts/version.ts
@@ -12,6 +12,6 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-export const version = "3.1.1";
+export const version = "3.1.2";
 
 export const cdiSupported = ["2.4"];
