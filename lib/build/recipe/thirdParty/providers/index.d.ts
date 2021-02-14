ae801ee69ccefbdb3240f3fd4d12263742dfa723
Bhumil Sarvaiya <sarvaiyabhumil@gmail.com>
2021-02-14 23:27:42 +0530

    third party provider facebook added

--- lib/build/recipe/thirdParty/providers/index.d.ts
+++ lib/build/recipe/thirdParty/providers/index.d.ts
@@ -1,2 +1,4 @@
 import ProviderGoogle from "./google";
+import ProviderFacebook from "./facebook";
 export declare let Google: typeof ProviderGoogle;
+export declare let Facebook: typeof ProviderFacebook;
--- lib/ts/recipe/thirdParty/providers/index.ts
+++ lib/ts/recipe/thirdParty/providers/index.ts
@@ -1,3 +1,5 @@
 import ProviderGoogle from "./google";
+import ProviderFacebook from "./facebook";
 
 export let Google = ProviderGoogle;
+export let Facebook = ProviderFacebook;
