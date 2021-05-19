d8944544c7be55ebbdb291324523d9d5589f8f7f
Rishabh Poddar <rishabh.poddar@gmail.com>
2021-05-19 21:56:36 +0530

    starts working on an example with ts

---
+++ examples/with-typescript/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import Supertokens from "../../";
+
+let app = express();
+
+// Supertokens.init({
+//     appInfo: {
+//         apiDomain: "",
+//         appName: "",
+//         websiteDomain: ""
+//     },
+//     recipeList: [],
+//     supertokens: {
+//         connectionURI: ""
+//     }
+// })
+
+app.listen();
