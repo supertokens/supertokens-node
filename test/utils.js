20bc90d6893c78bcd1a3c4759dd8fba148144503
Rishabh Poddar <rishabh.poddar@gmail.com>
2021-06-19 14:13:40 +0530

    removes unnecessary test files

--- test/utils.js
+++ test/utils.js
@@ -295,14 +295,6 @@ const consoleOptions = {
     cyan: 36,
 };
 
-module.exports.constants = {
-    AUTH0_DOMAIN: "dev-3myi6b3e.us.auth0.com",
-    AUTH0_CLIENT_ID: "wQwadNYL58PoXDTIEiLeBmCe89BnMfiv",
-    AUTH0_CLIENT_SECRET: "46n7WEAScmrHXukA_w6v1C8uYlVXPvAqEwu5eAyJRD5Wu951BHhiuE_0cQpW5GiV",
-    TEST_ID_TOKEN:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
-};
-
 module.exports.signUPRequest = async function (app, email, password) {
     return new Promise(function (resolve) {
         request(app)
