72de47a348b504776538c2ae5b2a8e63ac28ab27
Marek Dulowski <dulowski.marek@gmail.com>
2021-08-05 19:52:13 +0200

    Add happy path getUsersByEmail test

--- test/thirdparty/getUsersByEmailFeature.test.js
+++ test/thirdparty/getUsersByEmailFeature.test.js
@@ -3,11 +3,12 @@ let STExpress = require("../../");
 let assert = require("assert");
 let { ProcessState } = require("../../lib/build/processState");
 let ThirPartyRecipe = require("../../lib/build/recipe/thirdparty/recipe").default;
+const nock = require("nock");
 const express = require("express");
 const request = require("supertest");
 const { removeServerlessCache } = require("../../lib/build/utils");
 
-describe(`getUsersByEmail: ${printPath("[test/thirdparty/getUsersByEmailFeature.test.js]")}`, function () {
+describe.only(`getUsersByEmail: ${printPath("[test/thirdparty/getUsersByEmailFeature.test.js]")}`, function () {
     beforeEach(async function () {
         await killAllST();
         await setupST();
@@ -47,9 +48,9 @@ describe(`getUsersByEmail: ${printPath("[test/thirdparty/getUsersByEmailFeature.
         app.use(STExpress.middleware());
         app.use(STExpress.errorHandler());
 
-        const response = new Promise((resolve) => {
+        const response = await new Promise((resolve) => {
             request(app)
-                .get("/recipe/users/by-email?email=invalid@email")
+                .get("/auth/users/by-email?email=invalid@email")
                 .end((err, res) => {
                     if (err) {
                         return resolve(undefined);
@@ -60,6 +61,99 @@ describe(`getUsersByEmail: ${printPath("[test/thirdparty/getUsersByEmailFeature.
         });
 
         assert.notStrictEqual(response, undefined);
-        assert.strictEqual(response.statusCode, 500);
+        assert.strictEqual(response.status, 400);
+    });
+
+    it("valid email yields a user", async function () {
+        await startST();
+
+        const MockThirdPartyProvider = {
+            id: "mock",
+            get: async (recipe, authorisationCode) => {
+                return {
+                    accessTokenAPI: {
+                        url: "https://test.com/oauth/token",
+                    },
+                    authorisationRedirect: {
+                        url: "https://test.com/oauth/auth",
+                    },
+                    getProfileInfo: async (authCodeResponse) => {
+                        return {
+                            id: "mockUserId",
+                            email: {
+                                id: "john.doe@example.com",
+                                isVerified: true,
+                            },
+                        };
+                    },
+                };
+            },
+        };
+
+        STExpress.init({
+            supertokens: {
+                connectionURI: "http://localhost:8080",
+            },
+            appInfo: {
+                apiDomain: "api.supertokens.io",
+                appName: "SuperTokens",
+                websiteDomain: "supertokens.io",
+            },
+            recipeList: [
+                ThirPartyRecipe.init({
+                    signInAndUpFeature: {
+                        providers: [MockThirdPartyProvider],
+                    },
+                }),
+            ],
+        });
+
+        const app = express();
+
+        app.use(STExpress.middleware());
+        app.use(STExpress.errorHandler());
+
+        nock("https://test.com").post("/oauth/token").reply(200, {});
+
+        const signUpResponse = await new Promise((resolve) => {
+            request(app)
+                .post("/auth/signinup")
+                .send({
+                    thirdPartyId: "mock",
+                    code: "abcdefghj",
+                    redirectURI: "http://127.0.0.1/callback",
+                })
+                .end((err, res) => {
+                    if (err) {
+                        resolve(undefined);
+                    } else {
+                        resolve(res);
+                    }
+                });
+        });
+
+        const { body: responseBody } = await new Promise((resolve) => {
+            request(app)
+                .get("/auth/users/by-email?email=john.doe@example.com")
+                .end((err, res) => {
+                    if (err) {
+                        return resolve(undefined);
+                    }
+
+                    resolve(res);
+                });
+        });
+
+        assert.strictEqual(responseBody.status, "OK");
+
+        const firstUser = responseBody.users[0];
+
+        assert.deepStrictEqual(firstUser.thirdParty, {
+            id: "mock",
+            userId: "mockUserId",
+        });
+        assert.deepStrictEqual(firstUser.email, "john.doe@example.com");
+        assert.notStrictEqual(firstUser.id, undefined);
+        assert.notStrictEqual(firstUser.timeJoined, undefined);
     });
 });
