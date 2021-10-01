26e3cad835edac968af1973364ec4608e0526f71
Rishabh <rishabh@supertokens.io>
2021-10-01 20:58:50 +0530

    modifies publish script

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -2,6 +2,11 @@ version=$(cat ../package.json | jq .version | tr -d '"')
 isLatest=`curl -s -X GET \
 "https://api.supertokens.io/0/driver/latest/check?password=$SUPERTOKENS_API_KEY&version=$version&name=node" \
 -H 'api-version: 0'`
+if [[ $? -ne 0 ]]
+then
+    echo "api not working... exiting!"
+    exit 1
+fi
 if [[ `echo $isLatest | jq .isLatest` == "true" ]]
 then
     cd ..
