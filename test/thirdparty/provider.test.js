c8a6163f3b06a6e7ce3a9bbe9fb2c182d950a31f
Rishabh <rishabh@supertokens.io>
2021-11-05 13:15:58 +0530

    adds tests for multiple providers and isDefault flag

--- test/thirdparty/provider.test.js
+++ test/thirdparty/provider.test.js
@@ -596,4 +596,118 @@ describe(`providerTest: ${printPath("[test/thirdparty/provider.test.js]")}`, fun
             }
         }
     });
+
+    it("test duplicate provider without any default", async function () {
+        await startST();
+        try {
+            STExpress.init({
+                supertokens: {
+                    connectionURI: "http://localhost:8080",
+                },
+                appInfo: {
+                    apiDomain: "api.supertokens.io",
+                    appName: "SuperTokens",
+                    websiteDomain: "supertokens.io",
+                },
+                recipeList: [
+                    ThirPartyRecipe.init({
+                        signInAndUpFeature: {
+                            providers: [
+                                ThirParty.Google({
+                                    clientId: "test",
+                                    clientSecret: "test-secret",
+                                    scope: ["test-scope-1", "test-scope-2"],
+                                }),
+                                ThirParty.Google({
+                                    clientId: "test",
+                                    clientSecret: "test-secret",
+                                    scope: ["test-scope-1", "test-scope-2"],
+                                }),
+                            ],
+                        },
+                    }),
+                ],
+            });
+            throw new Error("should fail");
+        } catch (err) {
+            assert(
+                err.message ===
+                    `The providers array has multiple entries for the same third party provider. Please mark one of them as the default one by using "isDefault: true".`
+            );
+        }
+    });
+
+    it("test duplicate provider with both default", async function () {
+        await startST();
+        try {
+            STExpress.init({
+                supertokens: {
+                    connectionURI: "http://localhost:8080",
+                },
+                appInfo: {
+                    apiDomain: "api.supertokens.io",
+                    appName: "SuperTokens",
+                    websiteDomain: "supertokens.io",
+                },
+                recipeList: [
+                    ThirPartyRecipe.init({
+                        signInAndUpFeature: {
+                            providers: [
+                                ThirParty.Google({
+                                    isDefault: true,
+                                    clientId: "test",
+                                    clientSecret: "test-secret",
+                                    scope: ["test-scope-1", "test-scope-2"],
+                                }),
+                                ThirParty.Google({
+                                    isDefault: true,
+                                    clientId: "test",
+                                    clientSecret: "test-secret",
+                                    scope: ["test-scope-1", "test-scope-2"],
+                                }),
+                            ],
+                        },
+                    }),
+                ],
+            });
+            throw new Error("should fail");
+        } catch (err) {
+            assert(
+                err.message ===
+                    `You have provided multiple third party providers that have the id: "google" and are marked as "isDefault: true". Please only mark one of them as isDefault.`
+            );
+        }
+    });
+    it("test duplicate provider with one default", async function () {
+        await startST();
+        STExpress.init({
+            supertokens: {
+                connectionURI: "http://localhost:8080",
+            },
+            appInfo: {
+                apiDomain: "api.supertokens.io",
+                appName: "SuperTokens",
+                websiteDomain: "supertokens.io",
+            },
+            recipeList: [
+                ThirPartyRecipe.init({
+                    signInAndUpFeature: {
+                        providers: [
+                            ThirParty.Google({
+                                clientId: "test",
+                                clientSecret: "test-secret",
+                                scope: ["test-scope-1", "test-scope-2"],
+                            }),
+                            ThirParty.Google({
+                                isDefault: true,
+                                clientId: "test",
+                                clientSecret: "test-secret",
+                                scope: ["test-scope-1", "test-scope-2"],
+                            }),
+                        ],
+                    },
+                }),
+            ],
+        });
+    });
 });
