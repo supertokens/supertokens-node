dc57f7dd2ccb056686e46736264339c606bdaf36
Bhumil Sarvaiya <sarvaiyabhumil@gmail.com>
2021-12-20 18:20:59 +0530

    fixes #230 (#234)

---
+++ types/index.d.ts
@@ -0,0 +1,10 @@
+export * from "../lib/build/types";
+/**
+ * 'export *' does not re-export a default.
+ * import NextJS from "supertokens-node/nextjs";
+ * the above import statement won't be possible unless either
+ * - user add "esModuleInterop": true in their tsconfig.json file
+ * - we do the following change:
+ */
+import * as _default from "../lib/build/types";
+export default _default;
