aa624b413d20fb5ee43874386f719aac6b6cb279
Bhumil <sarvaiyabhumil@gmail.com>
2022-07-01 18:27:31 +0530

    test: new tests added for email and sms resend api

--- test/passwordless/emailDelivery.test.js
+++ test/passwordless/emailDelivery.test.js
@@ -247,6 +247,7 @@ describe(`emailDelivery: ${printPath("[test/passwordless/emailDelivery.test.js]"
         let email = undefined;
         let codeLifetime = undefined;
         let userInputCode = undefined;
+        let urlWithLinkCode = undefined;
         let outerOverrideCalled = false;
         let sendRawEmailCalled = false;
         let getContentCalled = false;
@@ -401,6 +402,505 @@ describe(`emailDelivery: ${printPath("[test/passwordless/emailDelivery.test.js]"
             .set("rid", "passwordless")
             .send({
                 email: "test@example.com",
+            })
+            .expect(500);
+
+        process.env.TEST_MODE = "testing";
+
+        assert.strictEqual(appName, "SuperTokens");
+        assert.strictEqual(email, "test@example.com");
+        assert.notStrictEqual(urlWithLinkCode, undefined);
+        assert.notStrictEqual(userInputCode, undefined);
+        assert.notStrictEqual(codeLifetime, undefined);
+        assert(codeLifetime > 0);
+        assert.strictEqual(result.status, 500);
+        assert(message === "Request failed with status code 500");
+    });
+
+    it("test default backward compatibility api being called: resend code api", async function () {
+        await startST();
+        STExpress.init({
+            supertokens: {
+                connectionURI: "http://localhost:8080",
+            },
+            appInfo: {
+                apiDomain: "api.supertokens.io",
+                appName: "SuperTokens",
+                websiteDomain: "supertokens.io",
+            },
+            recipeList: [
+                Passwordless.init({
+                    contactMethod: "EMAIL",
+                    flowType: "USER_INPUT_CODE_AND_MAGIC_LINK",
+                }),
+                Session.init(),
+            ],
+            telemetry: false,
+        });
+
+        // run test if current CDI version >= 2.11
+        if (!(await isCDIVersionCompatible("2.11"))) {
+            return;
+        }
+
+        const app = express();
+        app.use(express.json());
+        app.use(middleware());
+        app.use(errorHandler());
+
+        let appName = undefined;
+        let email = undefined;
+        let codeLifetime = undefined;
+        let urlWithLinkCode = undefined;
+        let userInputCode = undefined;
+        let loginCalled = false;
+
+        nock("https://api.supertokens.io")
+            .post("/0/st/auth/passwordless/login")
+            .reply(200, (uri, body) => {
+                loginCalled = true;
+                return {};
+            });
+
+        process.env.TEST_MODE = "production";
+
+        let response = await supertest(app)
+            .post("/auth/signinup/code")
+            .set("rid", "passwordless")
+            .send({
+                email: "test@example.com",
+            })
+            .expect(200);
+        assert.strictEqual(loginCalled, true);
+
+        nock("https://api.supertokens.io")
+            .post("/0/st/auth/passwordless/login")
+            .reply(200, (uri, body) => {
+                appName = body.appName;
+                email = body.email;
+                codeLifetime = body.codeLifetime;
+                urlWithLinkCode = body.urlWithLinkCode;
+                userInputCode = body.userInputCode;
+                return {};
+            });
+
+        await supertest(app)
+            .post("/auth/signinup/code/resend")
+            .set("rid", "passwordless")
+            .send({
+                deviceId: response.body.deviceId,
+                preAuthSessionId: response.body.preAuthSessionId,
+            })
+            .expect(200);
+
+        process.env.TEST_MODE = "testing";
+
+        assert.strictEqual(appName, "SuperTokens");
+        assert.strictEqual(email, "test@example.com");
+        assert.notStrictEqual(urlWithLinkCode, undefined);
+        assert.notStrictEqual(userInputCode, undefined);
+        assert.notStrictEqual(codeLifetime, undefined);
+        assert(codeLifetime > 0);
+    });
+
+    it("test backward compatibility: resend code api", async function () {
+        await startST();
+        let email = undefined;
+        let codeLifetime = undefined;
+        let urlWithLinkCode = undefined;
+        let userInputCode = undefined;
+        let sendCustomEmailCalled = false;
+        STExpress.init({
+            supertokens: {
+                connectionURI: "http://localhost:8080",
+            },
+            appInfo: {
+                apiDomain: "api.supertokens.io",
+                appName: "SuperTokens",
+                websiteDomain: "supertokens.io",
+            },
+            recipeList: [
+                Passwordless.init({
+                    contactMethod: "EMAIL",
+                    flowType: "USER_INPUT_CODE_AND_MAGIC_LINK",
+                    createAndSendCustomEmail: async (input) => {
+                        /**
+                         * when the function is called for the first time,
+                         * it will be for signinup
+                         */
+                        if (sendCustomEmailCalled) {
+                            email = input.email;
+                            codeLifetime = input.codeLifetime;
+                            urlWithLinkCode = input.urlWithLinkCode;
+                            userInputCode = input.userInputCode;
+                        }
+                        sendCustomEmailCalled = true;
+                    },
+                }),
+                Session.init(),
+            ],
+            telemetry: false,
+        });
+
+        // run test if current CDI version >= 2.11
+        if (!(await isCDIVersionCompatible("2.11"))) {
+            return;
+        }
+
+        const app = express();
+        app.use(express.json());
+        app.use(middleware());
+        app.use(errorHandler());
+
+        let response = await supertest(app)
+            .post("/auth/signinup/code")
+            .set("rid", "passwordless")
+            .send({
+                email: "test@example.com",
+            })
+            .expect(200);
+        await delay(2);
+        assert.strictEqual(sendCustomEmailCalled, true);
+        assert.strictEqual(email, undefined);
+        assert.strictEqual(urlWithLinkCode, undefined);
+        assert.strictEqual(userInputCode, undefined);
+        assert.strictEqual(codeLifetime, undefined);
+
+        await supertest(app)
+            .post("/auth/signinup/code/resend")
+            .set("rid", "passwordless")
+            .send({
+                deviceId: response.body.deviceId,
+                preAuthSessionId: response.body.preAuthSessionId,
+            })
+            .expect(200);
+        await delay(2);
+        assert.strictEqual(email, "test@example.com");
+        assert.notStrictEqual(urlWithLinkCode, undefined);
+        assert.notStrictEqual(userInputCode, undefined);
+        assert.notStrictEqual(codeLifetime, undefined);
+        assert(codeLifetime > 0);
+    });
+
+    it("test custom override: resend code api", async function () {
+        await startST();
+        let email = undefined;
+        let codeLifetime = undefined;
+        let urlWithLinkCode = undefined;
+        let userInputCode = undefined;
+        let type = undefined;
+        let appName = undefined;
+        let overrideCalled = false;
+        let loginCalled = false;
+        STExpress.init({
+            supertokens: {
+                connectionURI: "http://localhost:8080",
+            },
+            appInfo: {
+                apiDomain: "api.supertokens.io",
+                appName: "SuperTokens",
+                websiteDomain: "supertokens.io",
+            },
+            recipeList: [
+                Passwordless.init({
+                    contactMethod: "EMAIL",
+                    flowType: "USER_INPUT_CODE_AND_MAGIC_LINK",
+                    emailDelivery: {
+                        override: (oI) => {
+                            return {
+                                sendEmail: async (input) => {
+                                    /**
+                                     * when the function is called for the first time,
+                                     * it will be for signinup
+                                     */
+                                    if (overrideCalled) {
+                                        email = input.email;
+                                        urlWithLinkCode = input.urlWithLinkCode;
+                                        userInputCode = input.userInputCode;
+                                        codeLifetime = input.codeLifetime;
+                                        type = input.type;
+                                    }
+                                    overrideCalled = true;
+                                    await oI.sendEmail(input);
+                                },
+                            };
+                        },
+                    },
+                }),
+                Session.init(),
+            ],
+            telemetry: false,
+        });
+
+        // run test if current CDI version >= 2.11
+        if (!(await isCDIVersionCompatible("2.11"))) {
+            return;
+        }
+
+        const app = express();
+        app.use(express.json());
+        app.use(middleware());
+        app.use(errorHandler());
+
+        process.env.TEST_MODE = "production";
+
+        nock("https://api.supertokens.io")
+            .post("/0/st/auth/passwordless/login")
+            .reply(200, (uri, body) => {
+                loginCalled = true;
+                return {};
+            });
+
+        let response = await supertest(app)
+            .post("/auth/signinup/code")
+            .set("rid", "passwordless")
+            .send({
+                email: "test@example.com",
+            })
+            .expect(200);
+        assert.strictEqual(loginCalled, true);
+        assert.strictEqual(overrideCalled, true);
+
+        nock("https://api.supertokens.io")
+            .post("/0/st/auth/passwordless/login")
+            .reply(200, (uri, body) => {
+                appName = body.appName;
+                return {};
+            });
+
+        await supertest(app)
+            .post("/auth/signinup/code/resend")
+            .set("rid", "passwordless")
+            .send({
+                deviceId: response.body.deviceId,
+                preAuthSessionId: response.body.preAuthSessionId,
+            })
+            .expect(200);
+
+        process.env.TEST_MODE = "testing";
+
+        await delay(2);
+        assert.strictEqual(email, "test@example.com");
+        assert.strictEqual(appName, "SuperTokens");
+        assert.strictEqual(type, "PASSWORDLESS_LOGIN");
+        assert.notStrictEqual(urlWithLinkCode, undefined);
+        assert.notStrictEqual(userInputCode, undefined);
+        assert.notStrictEqual(codeLifetime, undefined);
+        assert(codeLifetime > 0);
+    });
+
+    it("test smtp service: resend code api", async function () {
+        await startST();
+        let email = undefined;
+        let codeLifetime = undefined;
+        let urlWithLinkCode = undefined;
+        let userInputCode = undefined;
+        let outerOverrideCalled = false;
+        let sendRawEmailCalled = false;
+        let getContentCalled = false;
+        STExpress.init({
+            supertokens: {
+                connectionURI: "http://localhost:8080",
+            },
+            appInfo: {
+                apiDomain: "api.supertokens.io",
+                appName: "SuperTokens",
+                websiteDomain: "supertokens.io",
+            },
+            recipeList: [
+                Passwordless.init({
+                    contactMethod: "EMAIL",
+                    flowType: "USER_INPUT_CODE_AND_MAGIC_LINK",
+                    emailDelivery: {
+                        service: new STMPService({
+                            smtpSettings: {
+                                host: "",
+                                from: {
+                                    email: "",
+                                    name: "",
+                                },
+                                password: "",
+                                port: 465,
+                                secure: true,
+                            },
+                            override: (oI) => {
+                                return {
+                                    sendRawEmail: async (input) => {
+                                        /**
+                                         * when the function is called for the first time,
+                                         * it will be for signinup
+                                         */
+                                        if (sendRawEmailCalled) {
+                                            assert.strictEqual(input.body, userInputCode);
+                                            assert.strictEqual(input.subject, urlWithLinkCode);
+                                            assert.strictEqual(input.toEmail, "test@example.com");
+                                            email = input.toEmail;
+                                        }
+                                        sendRawEmailCalled = true;
+                                    },
+                                    getContent: async (input) => {
+                                        /**
+                                         * when the function is called for the first time,
+                                         * it will be for signinup
+                                         */
+                                        if (getContentCalled) {
+                                            userInputCode = input.userInputCode;
+                                            urlWithLinkCode = input.urlWithLinkCode;
+                                            codeLifetime = input.codeLifetime;
+                                        }
+                                        getContentCalled = true;
+                                        assert.strictEqual(input.type, "PASSWORDLESS_LOGIN");
+                                        return {
+                                            body: input.userInputCode,
+                                            toEmail: input.email,
+                                            subject: input.urlWithLinkCode,
+                                        };
+                                    },
+                                };
+                            },
+                        }),
+                        override: (oI) => {
+                            return {
+                                sendEmail: async (input) => {
+                                    outerOverrideCalled = true;
+                                    await oI.sendEmail(input);
+                                },
+                            };
+                        },
+                    },
+                }),
+                Session.init(),
+            ],
+            telemetry: false,
+        });
+
+        // run test if current CDI version >= 2.11
+        if (!(await isCDIVersionCompatible("2.11"))) {
+            return;
+        }
+
+        const app = express();
+        app.use(express.json());
+        app.use(middleware());
+        app.use(errorHandler());
+
+        let response = await supertest(app)
+            .post("/auth/signinup/code")
+            .set("rid", "passwordless")
+            .send({
+                email: "test@example.com",
+            })
+            .expect(200);
+        await delay(2);
+        assert(getContentCalled);
+        assert(sendRawEmailCalled);
+        assert.strictEqual(email, undefined);
+        assert.strictEqual(urlWithLinkCode, undefined);
+        assert.strictEqual(userInputCode, undefined);
+        assert.strictEqual(codeLifetime, undefined);
+
+        await supertest(app)
+            .post("/auth/signinup/code/resend")
+            .set("rid", "passwordless")
+            .send({
+                deviceId: response.body.deviceId,
+                preAuthSessionId: response.body.preAuthSessionId,
+            })
+            .expect(200);
+        await delay(2);
+
+        assert.strictEqual(email, "test@example.com");
+        assert(outerOverrideCalled);
+        assert.notStrictEqual(urlWithLinkCode, undefined);
+        assert.notStrictEqual(userInputCode, undefined);
+        assert.notStrictEqual(codeLifetime, undefined);
+        assert(codeLifetime > 0);
+    });
+
+    it("test default backward compatibility api being called, error message sent back to user: resend code api", async function () {
+        await startST();
+        STExpress.init({
+            supertokens: {
+                connectionURI: "http://localhost:8080",
+            },
+            appInfo: {
+                apiDomain: "api.supertokens.io",
+                appName: "SuperTokens",
+                websiteDomain: "supertokens.io",
+            },
+            recipeList: [
+                Passwordless.init({
+                    contactMethod: "EMAIL",
+                    flowType: "USER_INPUT_CODE_AND_MAGIC_LINK",
+                }),
+                Session.init(),
+            ],
+            telemetry: false,
+        });
+
+        // run test if current CDI version >= 2.11
+        if (!(await isCDIVersionCompatible("2.11"))) {
+            return;
+        }
+
+        const app = express();
+        app.use(express.json());
+        app.use(middleware());
+        app.use(errorHandler());
+        let message = "";
+        app.use((err, req, res, next) => {
+            message = err.message;
+            res.status(500).send(message);
+        });
+
+        let appName = undefined;
+        let email = undefined;
+        let codeLifetime = undefined;
+        let urlWithLinkCode = undefined;
+        let userInputCode = undefined;
+        let loginCalled = false;
+
+        nock("https://api.supertokens.io")
+            .post("/0/st/auth/passwordless/login")
+            .reply(200, (uri, body) => {
+                loginCalled = true;
+                return {};
+            });
+
+        process.env.TEST_MODE = "production";
+
+        let response = await supertest(app)
+            .post("/auth/signinup/code")
+            .set("rid", "passwordless")
+            .send({
+                email: "test@example.com",
+            })
+            .expect(200);
+
+        assert.strictEqual(appName, undefined);
+        assert.strictEqual(email, undefined);
+        assert.strictEqual(urlWithLinkCode, undefined);
+        assert.strictEqual(userInputCode, undefined);
+        assert.strictEqual(codeLifetime, undefined);
+        assert.strictEqual(loginCalled, true);
+
+        nock("https://api.supertokens.io")
+            .post("/0/st/auth/passwordless/login")
+            .reply(500, (uri, body) => {
+                appName = body.appName;
+                email = body.email;
+                codeLifetime = body.codeLifetime;
+                urlWithLinkCode = body.urlWithLinkCode;
+                userInputCode = body.userInputCode;
+                return {};
+            });
+
+        let result = await supertest(app)
+            .post("/auth/signinup/code/resend")
+            .set("rid", "passwordless")
+            .send({
+                deviceId: response.body.deviceId,
+                preAuthSessionId: response.body.preAuthSessionId,
             })
             .expect(500);
 
