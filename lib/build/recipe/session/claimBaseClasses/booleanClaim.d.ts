a719f390a20045a54086c2867ad4e116772d5bc6
Mihaly Lengyel <mihaly@lengyel.tech>
2022-07-06 00:33:31 +0200

    feat: add isTrue and isFalse validators

--- lib/build/recipe/session/claimBaseClasses/booleanClaim.d.ts
+++ lib/build/recipe/session/claimBaseClasses/booleanClaim.d.ts
@@ -1,6 +1,10 @@
 // @ts-nocheck
-import { SessionClaim } from "../types";
+import { SessionClaim, SessionClaimValidator } from "../types";
 import { PrimitiveClaim } from "./primitiveClaim";
 export declare class BooleanClaim extends PrimitiveClaim<boolean> {
     constructor(conf: { key: string; fetchValue: SessionClaim<boolean>["fetchValue"] });
+    validators: PrimitiveClaim<boolean>["validators"] & {
+        isTrue: (maxAge?: number) => SessionClaimValidator;
+        isFalse: (maxAge?: number) => SessionClaimValidator;
+    };
 }
--- lib/ts/recipe/session/claimBaseClasses/booleanClaim.ts
+++ lib/ts/recipe/session/claimBaseClasses/booleanClaim.ts
@@ -1,8 +1,29 @@
-import { SessionClaim } from "../types";
+import { SessionClaim, SessionClaimValidator } from "../types";
 import { PrimitiveClaim } from "./primitiveClaim";
 
 export class BooleanClaim extends PrimitiveClaim<boolean> {
     constructor(conf: { key: string; fetchValue: SessionClaim<boolean>["fetchValue"] }) {
         super(conf);
+
+        this.validators = {
+            ...this.validators,
+            isTrue: (maxAge?: number): SessionClaimValidator => {
+                if (maxAge) {
+                    return this.validators.hasFreshValue(true, maxAge);
+                }
+                return this.validators.hasValue(true);
+            },
+            isFalse: (maxAge?: number): SessionClaimValidator => {
+                if (maxAge) {
+                    return this.validators.hasFreshValue(false, maxAge);
+                }
+                return this.validators.hasValue(false);
+            },
+        };
     }
+
+    validators!: PrimitiveClaim<boolean>["validators"] & {
+        isTrue: (maxAge?: number) => SessionClaimValidator;
+        isFalse: (maxAge?: number) => SessionClaimValidator;
+    };
 }
