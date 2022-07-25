a7915beeed2b36395d28fa428c93dc030695e1a4
Nemi Shah <nemishah1212@gmail.com>
2022-07-25 18:34:31 +0530

    Fetch user meta data when returning users list

--- lib/ts/recipe/dashboard/api/usersGet.ts
+++ lib/ts/recipe/dashboard/api/usersGet.ts
@@ -1,5 +1,3 @@
-import { APIInterface, APIOptions } from "../types";
-
 /* Copyright (c) 2022, VRAI Labs and/or its affiliates. All rights reserved.
  *
  * This software is licensed under the Apache License, Version 2.0 (the
@@ -14,9 +12,12 @@ import { APIInterface, APIOptions } from "../types";
  * License for the specific language governing permissions and limitations
  * under the License.
  */
+import { APIInterface, APIOptions } from "../types";
 import STError from "../../../error";
 import SuperTokens from "../../../supertokens";
 import { send200Response } from "../../../utils";
+import UserMetaDataRecipe from "../../usermetadata/recipe";
+import UserMetaData from "../../usermetadata";
 
 export default async function usersGet(_: APIInterface, options: APIOptions): Promise<boolean> {
     const req = options.req;
@@ -44,11 +45,60 @@ export default async function usersGet(_: APIInterface, options: APIOptions): Pr
 
     let paginationToken = options.req.getKeyValueFromQuery("paginationToken");
 
-    const usersResponse = await SuperTokens.getInstanceOrThrowError().getUsers({
+    let usersResponse = await SuperTokens.getInstanceOrThrowError().getUsers({
         timeJoinedOrder: timeJoinedOrder,
         limit: parseInt(limit),
         paginationToken,
     });
+
+    // If the UserMetaData recipe has been initialised, fetch first and last name
+    try {
+        UserMetaDataRecipe.getInstanceOrThrowError();
+    } catch (e) {
+        // Recipe has not been initialised, return without first name and last name
+        send200Response(options.res, {
+            status: "OK",
+            ...usersResponse,
+        });
+        return true;
+    }
+
+    let updatedUsersArray: {
+        recipeId: string;
+        user: any;
+    }[] = [];
+    let metaDataFetchPromises: Promise<any>[] = [];
+
+    usersResponse.users.forEach((userObj) => {
+        metaDataFetchPromises.push(
+            new Promise(async (resolve, reject) => {
+                try {
+                    const userMetaDataResponse = await UserMetaData.getUserMetadata(userObj.user.id);
+                    const { first_name, last_name } = userMetaDataResponse.metadata;
+
+                    updatedUsersArray.push({
+                        ...userObj,
+                        user: {
+                            ...userObj.user,
+                            firstName: first_name,
+                            lastName: last_name,
+                        },
+                    });
+                    resolve(true);
+                } catch (e) {
+                    // Something went wrong when fetching user meta data
+                    reject(e);
+                }
+            })
+        );
+    });
+
+    await Promise.all(metaDataFetchPromises);
+
+    usersResponse = {
+        ...usersResponse,
+        users: updatedUsersArray,
+    };
 
     send200Response(options.res, {
         status: "OK",
