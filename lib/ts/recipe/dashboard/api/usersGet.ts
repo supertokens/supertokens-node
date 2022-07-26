3357fd9d93e625ea2bb2d3913d76704c141686a2
Nemi Shah <nemishah1212@gmail.com>
2022-07-26 14:21:03 +0530

    Update based on PR comments

--- lib/ts/recipe/dashboard/api/usersGet.ts
+++ lib/ts/recipe/dashboard/api/usersGet.ts
@@ -58,7 +58,8 @@ export default async function usersGet(_: APIInterface, options: APIOptions): Pr
         // Recipe has not been initialised, return without first name and last name
         send200Response(options.res, {
             status: "OK",
-            ...usersResponse,
+            users: usersResponse.users,
+            nextPaginationToken: usersResponse.nextPaginationToken,
         });
         return true;
     }
@@ -67,33 +68,66 @@ export default async function usersGet(_: APIInterface, options: APIOptions): Pr
         recipeId: string;
         user: any;
     }[] = [];
-    let metaDataFetchPromises: Promise<any>[] = [];
+    let metaDataFetchPromises: (() => Promise<any>)[] = [];
 
-    usersResponse.users.forEach((userObj) => {
+    for (let i = 0; i < usersResponse.users.length; i++) {
+        const userObj = usersResponse.users[i];
         metaDataFetchPromises.push(
-            new Promise(async (resolve, reject) => {
-                try {
-                    const userMetaDataResponse = await UserMetaData.getUserMetadata(userObj.user.id);
-                    const { first_name, last_name } = userMetaDataResponse.metadata;
+            (): Promise<any> =>
+                new Promise(async (resolve, reject) => {
+                    try {
+                        const userMetaDataResponse = await UserMetaData.getUserMetadata(userObj.user.id);
+                        const { first_name, last_name } = userMetaDataResponse.metadata;
 
-                    updatedUsersArray.push({
-                        ...userObj,
-                        user: {
-                            ...userObj.user,
-                            firstName: first_name,
-                            lastName: last_name,
-                        },
-                    });
-                    resolve(true);
-                } catch (e) {
-                    // Something went wrong when fetching user meta data
-                    reject(e);
-                }
-            })
+                        updatedUsersArray[i] = {
+                            recipeId: userObj.recipeId,
+                            user: {
+                                ...userObj.user,
+                                firstName: first_name,
+                                lastName: last_name,
+                            },
+                        };
+                        resolve(true);
+                    } catch (e) {
+                        // Something went wrong when fetching user meta data
+                        reject(e);
+                    }
+                })
         );
-    });
+    }
 
-    await Promise.all(metaDataFetchPromises);
+    let promiseArrayStartPosition = 0;
+    let batchSize = 5;
+
+    while (promiseArrayStartPosition < metaDataFetchPromises.length) {
+        /**
+         * We want to query only 5 in parallel at a time
+         *
+         * First we check if the the array has enough elements to iterate
+         * promiseArrayStartPosition + 4 (5 elements including current)
+         */
+        let promiseArrayEndPosition = promiseArrayStartPosition + (batchSize - 1);
+
+        // If the end position is higher than the arrays length, we need to adjust it
+        if (promiseArrayEndPosition >= metaDataFetchPromises.length) {
+            /**
+             * For example if the array has 7 elements [A, B, C, D, E, F, G], when you run
+             * the second batch [startPosition = 5], this will result in promiseArrayEndPosition
+             * to be equal to 6 [5 + ((7 - 1) - 5)] and will then iterate over indexes [5] and [6]
+             */
+            promiseArrayEndPosition =
+                promiseArrayStartPosition + (metaDataFetchPromises.length - 1 - promiseArrayStartPosition);
+        }
+
+        let promisesToCall: (() => Promise<any>)[] = [];
+
+        for (let j = promiseArrayStartPosition; j <= promiseArrayEndPosition; j++) {
+            promisesToCall.push(metaDataFetchPromises[j]);
+        }
+
+        await Promise.all(promisesToCall.map((p) => p()));
+        promiseArrayStartPosition += batchSize;
+    }
 
     usersResponse = {
         ...usersResponse,
@@ -102,7 +136,8 @@ export default async function usersGet(_: APIInterface, options: APIOptions): Pr
 
     send200Response(options.res, {
         status: "OK",
-        ...usersResponse,
+        users: usersResponse.users,
+        nextPaginationToken: usersResponse.nextPaginationToken,
     });
 
     return true;
