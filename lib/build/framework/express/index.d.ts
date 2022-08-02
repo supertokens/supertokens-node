add421a4857c67de536b73616f71274734c1561d
Bhumil Sarvaiya <sarvaiyabhumil@gmail.com>
2022-08-02 11:12:28 +0530

    fix: bug fix while parsing nextjs request to fetch form data (#363)
    * fix: bug fix while parsing nextjs request to fetch form data. Closes #362
    
    * added test for PR

--- lib/build/framework/express/index.d.ts
+++ lib/build/framework/express/index.d.ts
@@ -1,14 +1,27 @@
 // @ts-nocheck
+/// <reference types="qs" />
 export type { SessionRequest } from "./framework";
 export declare const middleware: () => (
-    req: import("express").Request,
-    res: import("express").Response,
+    req: import("express").Request<
+        import("express-serve-static-core").ParamsDictionary,
+        any,
+        any,
+        import("qs").ParsedQs,
+        Record<string, any>
+    >,
+    res: import("express").Response<any, Record<string, any>>,
     next: import("express").NextFunction
 ) => Promise<void>;
 export declare const errorHandler: () => (
     err: any,
-    req: import("express").Request,
-    res: import("express").Response,
+    req: import("express").Request<
+        import("express-serve-static-core").ParamsDictionary,
+        any,
+        any,
+        import("qs").ParsedQs,
+        Record<string, any>
+    >,
+    res: import("express").Response<any, Record<string, any>>,
     next: import("express").NextFunction
 ) => Promise<void>;
 export declare const wrapRequest: (unwrapped: any) => import("..").BaseRequest;
