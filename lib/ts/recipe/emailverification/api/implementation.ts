3f5e26469ff1883463c0cd06bf4a561392a4e6eb
rishabhpoddar <rishabh.poddar@gmail.com>
2022-11-09 12:23:33 +0530

    makes changes to the API code

--- lib/ts/recipe/emailverification/api/implementation.ts
+++ lib/ts/recipe/emailverification/api/implementation.ts
@@ -102,10 +102,23 @@ export default function getAPIInterface(): APIInterface {
                 });
 
                 if (response.status === "EMAIL_ALREADY_VERIFIED_ERROR") {
+                    if ((await session.getClaimValue(EmailVerificationClaim)) !== true) {
+                        // this can happen if the email was verified in another browser
+                        // and this session is still outdated - and the user has not
+                        // called the get email verification API yet.
+                        await session.fetchAndSetClaim(EmailVerificationClaim, userContext);
+                    }
                     logDebugMessage(
                         `Email verification email not sent to ${emailInfo.email} because it is already verified.`
                     );
                     return response;
+                }
+
+                if ((await session.getClaimValue(EmailVerificationClaim)) !== false) {
+                    // this can happen if the email was unverified in another browser
+                    // and this session is still outdated - and the user has not
+                    // called the get email verification API yet.
+                    await session.fetchAndSetClaim(EmailVerificationClaim, userContext);
                 }
 
                 let emailVerifyLink =
