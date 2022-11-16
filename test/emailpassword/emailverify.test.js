141345f94d1082fc5ca876da236f82ee461b36dd
Rishabh Poddar <rishabh.poddar@gmail.com>
2022-11-16 11:44:23 +0530

    Email verification post session claim update (#420)
    * makes changes to the API code
    
    * adds test

--- test/emailpassword/emailverify.test.js
+++ test/emailpassword/emailverify.test.js
@@ -1455,4 +1455,104 @@ describe(`emailverify: ${printPath("[test/emailpassword/emailverify.test.js]")}`
         assert.ok(caughtError);
         assert.strictEqual(caughtError.message, "Unknown User ID provided without email");
     });
+
+    it("test that generate email verification token API updates session claims", async function () {
+        await startST();
+
+        STExpress.init({
+            supertokens: {
+                connectionURI: "http://localhost:8080",
+            },
+            appInfo: {
+                apiDomain: "api.supertokens.io",
+                appName: "SuperTokens",
+                websiteDomain: "supertokens.io",
+            },
+            recipeList: [
+                EmailPassword.init(),
+                EmailVerification.init({
+                    mode: "OPTIONAL",
+                    createAndSendCustomEmail: (user, emailVerificationURLWithToken) => {},
+                }),
+                Session.init({
+                    antiCsrf: "VIA_TOKEN",
+                }),
+            ],
+        });
+
+        const app = express();
+
+        app.use(middleware());
+
+        app.use(errorHandler());
+
+        const querier = Querier.getNewInstanceOrThrowError(undefined);
+        const apiVersion = await querier.getAPIVersion();
+        if (maxVersion(apiVersion, "2.10") !== apiVersion) {
+            return this.skip();
+        }
+
+        let response = await signUPRequest(app, "test@gmail.com", "testPass123");
+        assert.strictEqual(response.body.status, "OK");
+        assert.strictEqual(response.status, 200);
+
+        let userId = response.body.user.id;
+        let infoFromResponse = extractInfoFromResponse(response);
+        let antiCsrfToken = infoFromResponse.antiCsrf;
+        let token = await EmailVerification.createEmailVerificationToken(userId);
+        await EmailVerification.verifyEmailUsingToken(token.token);
+        response = await emailVerifyTokenRequest(
+            app,
+            infoFromResponse.accessToken,
+            infoFromResponse.idRefreshTokenFromCookie,
+            antiCsrfToken,
+            userId
+        );
+        infoFromResponse = extractInfoFromResponse(response);
+        assert.strictEqual(response.statusCode, 200);
+        assert.deepStrictEqual(response.body.status, "EMAIL_ALREADY_VERIFIED_ERROR");
+        let frontendInfo = JSON.parse(new Buffer.from(infoFromResponse.frontToken, "base64").toString());
+        assert.strictEqual(frontendInfo.up["st-ev"].v, true);
+
+        // calling the API again should not modify the access token again
+        response = await emailVerifyTokenRequest(
+            app,
+            infoFromResponse.accessToken,
+            infoFromResponse.idRefreshTokenFromCookie,
+            antiCsrfToken,
+            userId
+        );
+        let infoFromResponse2 = extractInfoFromResponse(response);
+        assert.strictEqual(response.statusCode, 200);
+        assert.deepStrictEqual(response.body.status, "EMAIL_ALREADY_VERIFIED_ERROR");
+        assert.strictEqual(infoFromResponse2.frontToken, undefined);
+
+        // now we mark the email as unverified and try again
+        await EmailVerification.unverifyEmail(userId);
+        response = await emailVerifyTokenRequest(
+            app,
+            infoFromResponse.accessToken,
+            infoFromResponse.idRefreshTokenFromCookie,
+            antiCsrfToken,
+            userId
+        );
+        infoFromResponse = extractInfoFromResponse(response);
+        assert.strictEqual(response.statusCode, 200);
+        assert.deepStrictEqual(response.body.status, "OK");
+        frontendInfo = JSON.parse(new Buffer.from(infoFromResponse.frontToken, "base64").toString());
+        assert.strictEqual(frontendInfo.up["st-ev"].v, false);
+
+        // calling the API again should not modify the access token again
+        response = await emailVerifyTokenRequest(
+            app,
+            infoFromResponse.accessToken,
+            infoFromResponse.idRefreshTokenFromCookie,
+            antiCsrfToken,
+            userId
+        );
+        infoFromResponse2 = extractInfoFromResponse(response);
+        assert.strictEqual(response.statusCode, 200);
+        assert.deepStrictEqual(response.body.status, "OK");
+        assert.strictEqual(infoFromResponse2.frontToken, undefined);
+    });
 });
