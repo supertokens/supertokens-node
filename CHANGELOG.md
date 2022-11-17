3425faa10dfe5699ecc915059b97d92c819f27fc
Nemi Shah <nemishah1212@gmail.com>
2022-11-17 18:31:30 +0530

    Update package version and Changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [unreleased]
 
-## [12.0.7] - 2022-11-17
+## [12.1.0] - 2022-11-17
 
 ### Added:
 
--- lib/build/version.d.ts
+++ lib/build/version.d.ts
@@ -1,4 +1,4 @@
 // @ts-nocheck
-export declare const version = "12.0.7";
+export declare const version = "12.1.0";
 export declare const cdiSupported: string[];
 export declare const dashboardVersion = "0.2";
