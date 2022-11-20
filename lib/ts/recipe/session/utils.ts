7a217af3687f902c1fca51623fa58395f04bdc4e
Mihaly Lengyel <mihaly@lengyel.tech>
2022-11-21 00:09:18 +0100

    feat: update additional expiration of access token cookies to 100 years

--- lib/ts/recipe/session/utils.ts
+++ lib/ts/recipe/session/utils.ts
@@ -273,11 +273,11 @@ export function attachCreateOrRefreshSessionResponseToExpressRes(
         res,
         "access",
         accessToken.token,
-        // We set the expiration to 10 years, because we can't really access the expiration of the refresh token everywhere we are setting it.
+        // We set the expiration to 100 years, because we can't really access the expiration of the refresh token everywhere we are setting it.
         // This should be safe to do, since this is only the validity of the cookie (set here or on the frontend) but we check the expiration of the JWT anyway.
         // Even if the token is expired the presence of the token indicates that the user could have a valid refresh
         // Setting them to infinity would require special case handling on the frontend and just adding 10 years seems enough.
-        Date.now() + 315360000000,
+        Date.now() + 3153600000000,
         userContext
     );
     setToken(config, req, res, "refresh", refreshToken.token, refreshToken.expiry, userContext);
