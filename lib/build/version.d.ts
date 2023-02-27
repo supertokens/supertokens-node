a4ffd55c471547e8e4a670b14529e3f6f6fc9c89
Mihály Lengyel <mihaly@lengyel.tech>
2023-02-27 17:32:17 +0100

    fix: form parsing of apple sign-ins in nextjs (#499)

--- lib/build/version.d.ts
+++ lib/build/version.d.ts
@@ -1,4 +1,4 @@
 // @ts-nocheck
-export declare const version = "13.1.1";
+export declare const version = "13.1.2";
 export declare const cdiSupported: string[];
 export declare const dashboardVersion = "0.4";
