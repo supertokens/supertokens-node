5b3d5c1d0742f4d1d13c514584a74c65f06f10e4
productdevbook <hi@productdevbook.com>
2023-05-13 05:57:52 +0300

    Create tsup.config.ts

---
+++ tsup.config.ts
@@ -0,0 +1,22 @@
+import type { Options } from 'tsup'
+
+import pkg from './package.json'
+const external = [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+]
+
+export default <Options>{
+    entryPoints: [
+        'lib/ts/**/*.ts',
+    ],
+    outDir: 'dist',
+    format: ['esm', 'cjs'],
+    target: 'node14',
+    dts: true,
+    external,
+    clean: true,
+    esbuildOptions: (options) => {
+        options.chunkNames = `__chunk/[name]-[hash]`;
+    }
+}
