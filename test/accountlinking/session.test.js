acb39fa36d930dd8877e5a87455bdd1ec626078f
rishabhpoddar <rishabh.poddar@gmail.com>
2023-05-30 14:09:46 +0530

    adds more tests

--- test/accountlinking/session.test.js
+++ test/accountlinking/session.test.js
@@ -269,4 +269,253 @@ describe(`sessionTests: ${printPath("[test/accountlinking/session.test.js]")}`, 
             assert(session.getUserId() === "random");
         });
     });
+
+    describe("getSessionWithoutRequestResponse tests", function () {
+        it("getSessionWithoutRequestResponse with no linked accounts should have same user id and recipe id", async function () {
+            await startST();
+            supertokens.init({
+                supertokens: {
+                    connectionURI: "http://localhost:8080",
+                },
+                appInfo: {
+                    apiDomain: "api.supertokens.io",
+                    appName: "SuperTokens",
+                    websiteDomain: "supertokens.io",
+                },
+                recipeList: [EmailPassword.init(), Session.init()],
+            });
+
+            let epUser = (await EmailPassword.signUp("test@example.com", "password123")).user;
+
+            let session = await Session.createNewSessionWithoutRequestResponse(epUser.loginMethods[0].recipeUserId);
+
+            session = await Session.getSessionWithoutRequestResponse(session.getAccessToken());
+
+            assert(session.getUserId() === session.getRecipeUserId().getAsString());
+        });
+
+        it("getSessionWithoutRequestResponse with linked accounts should have different user id and recipe id", async function () {
+            await startST();
+            supertokens.init({
+                supertokens: {
+                    connectionURI: "http://localhost:8080",
+                },
+                appInfo: {
+                    apiDomain: "api.supertokens.io",
+                    appName: "SuperTokens",
+                    websiteDomain: "supertokens.io",
+                },
+                recipeList: [EmailPassword.init(), Session.init()],
+            });
+
+            let epUser = (await EmailPassword.signUp("test@example.com", "password123")).user;
+            await AccountLinking.createPrimaryUser(epUser.loginMethods[0].recipeUserId);
+
+            let epUser2 = (await EmailPassword.signUp("test2@example.com", "password123")).user;
+
+            await AccountLinking.linkAccounts(epUser2.loginMethods[0].recipeUserId, epUser.id);
+
+            let session = await Session.createNewSessionWithoutRequestResponse(epUser2.loginMethods[0].recipeUserId);
+
+            session = await Session.getSessionWithoutRequestResponse(session.getAccessToken());
+
+            assert(session.getUserId() !== session.getRecipeUserId().getAsString());
+            assert(session.getUserId() === epUser.id);
+            assert(session.getRecipeUserId().getAsString() === epUser2.id);
+        });
+
+        it("getSessionWithoutRequestResponse with no linked and no auth recipe accounts should have same user id and recipe id", async function () {
+            await startST();
+            supertokens.init({
+                supertokens: {
+                    connectionURI: "http://localhost:8080",
+                },
+                appInfo: {
+                    apiDomain: "api.supertokens.io",
+                    appName: "SuperTokens",
+                    websiteDomain: "supertokens.io",
+                },
+                recipeList: [EmailPassword.init(), Session.init()],
+            });
+
+            let session = await Session.createNewSessionWithoutRequestResponse(
+                supertokens.convertToRecipeUserId("random")
+            );
+
+            session = await Session.getSessionWithoutRequestResponse(session.getAccessToken());
+
+            assert(session.getUserId() === session.getRecipeUserId().getAsString());
+            assert(session.getUserId() === "random");
+        });
+    });
+
+    describe("getSession tests", function () {
+        it("get session with no linked accounts should have same user id and recipe id", async function () {
+            await startST();
+            let userId = undefined;
+            let recipeUserId = undefined;
+            supertokens.init({
+                supertokens: {
+                    connectionURI: "http://localhost:8080",
+                },
+                appInfo: {
+                    apiDomain: "api.supertokens.io",
+                    appName: "SuperTokens",
+                    websiteDomain: "supertokens.io",
+                },
+                recipeList: [EmailPassword.init(), Session.init()],
+            });
+
+            let epUser = (await EmailPassword.signUp("test@example.com", "password123")).user;
+
+            let session = await Session.createNewSessionWithoutRequestResponse(
+                epUser.loginMethods[0].recipeUserId,
+                {},
+                {}
+            );
+
+            const app = express();
+
+            app.use(middleware());
+
+            app.post("/getsession", async (req, res) => {
+                let session = await Session.getSession(req, res);
+                userId = session.getUserId();
+                recipeUserId = session.getRecipeUserId().getAsString();
+                res.status(200).send("");
+            });
+
+            app.use(errorHandler());
+
+            let res = await new Promise((resolve) =>
+                request(app)
+                    .post("/getsession")
+                    .set("Cookie", ["sAccessToken=" + session.getAccessToken()])
+                    .expect(200)
+                    .end((err, res) => {
+                        if (err) {
+                            resolve(undefined);
+                        } else {
+                            resolve(res);
+                        }
+                    })
+            );
+
+            assert(userId === recipeUserId && userId !== undefined);
+        });
+
+        it("get session with linked accounts should have different user id and recipe id", async function () {
+            await startST();
+            let userId = undefined;
+            let recipeUserId = undefined;
+            supertokens.init({
+                supertokens: {
+                    connectionURI: "http://localhost:8080",
+                },
+                appInfo: {
+                    apiDomain: "api.supertokens.io",
+                    appName: "SuperTokens",
+                    websiteDomain: "supertokens.io",
+                },
+                recipeList: [EmailPassword.init(), Session.init()],
+            });
+
+            let epUser = (await EmailPassword.signUp("test@example.com", "password123")).user;
+            await AccountLinking.createPrimaryUser(epUser.loginMethods[0].recipeUserId);
+
+            let epUser2 = (await EmailPassword.signUp("test2@example.com", "password123")).user;
+
+            await AccountLinking.linkAccounts(epUser2.loginMethods[0].recipeUserId, epUser.id);
+
+            let session = await Session.createNewSessionWithoutRequestResponse(
+                epUser2.loginMethods[0].recipeUserId,
+                {},
+                {}
+            );
+
+            const app = express();
+
+            app.use(middleware());
+
+            app.post("/getsession", async (req, res) => {
+                let session = await Session.getSession(req, res);
+                userId = session.getUserId();
+                recipeUserId = session.getRecipeUserId().getAsString();
+                res.status(200).send("");
+            });
+
+            app.use(errorHandler());
+
+            let res = await new Promise((resolve) =>
+                request(app)
+                    .post("/getsession")
+                    .set("Cookie", ["sAccessToken=" + session.getAccessToken()])
+                    .expect(200)
+                    .end((err, res) => {
+                        if (err) {
+                            resolve(undefined);
+                        } else {
+                            resolve(res);
+                        }
+                    })
+            );
+
+            assert(userId !== recipeUserId);
+            assert(userId === epUser.id);
+            assert(recipeUserId === epUser2.id);
+        });
+
+        it("get session with no linked accounts and no auth recipe should have same user id and recipe id", async function () {
+            await startST();
+            let userId = undefined;
+            let recipeUserId = undefined;
+            supertokens.init({
+                supertokens: {
+                    connectionURI: "http://localhost:8080",
+                },
+                appInfo: {
+                    apiDomain: "api.supertokens.io",
+                    appName: "SuperTokens",
+                    websiteDomain: "supertokens.io",
+                },
+                recipeList: [EmailPassword.init(), Session.init()],
+            });
+
+            let session = await Session.createNewSessionWithoutRequestResponse(
+                supertokens.convertToRecipeUserId("random"),
+                {},
+                {}
+            );
+
+            const app = express();
+
+            app.use(middleware());
+
+            app.post("/getsession", async (req, res) => {
+                let session = await Session.getSession(req, res);
+                userId = session.getUserId();
+                recipeUserId = session.getRecipeUserId().getAsString();
+                res.status(200).send("");
+            });
+
+            app.use(errorHandler());
+
+            let res = await new Promise((resolve) =>
+                request(app)
+                    .post("/getsession")
+                    .set("Cookie", ["sAccessToken=" + session.getAccessToken()])
+                    .expect(200)
+                    .end((err, res) => {
+                        if (err) {
+                            resolve(undefined);
+                        } else {
+                            resolve(res);
+                        }
+                    })
+            );
+
+            assert(userId === recipeUserId && userId !== undefined);
+            assert(userId === "random");
+        });
+    });
 });
