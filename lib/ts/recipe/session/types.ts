6e3143b95fffeaa54b78b7c0c0374f8f8ec625ad
iresharma <iresh.sharma8@gmail.com>
2023-05-31 12:05:38 +0530

    type fix

--- lib/ts/recipe/session/types.ts
+++ lib/ts/recipe/session/types.ts
@@ -74,11 +74,7 @@ export type TypeInput = {
     }) => TokenTransferMethod | "any";
 
     errorHandlers?: ErrorHandlers;
-    antiCsrf?:
-        | "VIA_TOKEN"
-        | "VIA_CUSTOM_HEADER"
-        | "NONE"
-        | ((req: BaseRequest, userContext: any) => Promise<AntiCsrfType>);
+    antiCsrf?: AntiCsrfType | ((req: BaseRequest, userContext: any) => Promise<AntiCsrfType>);
     exposeAccessTokenToFrontendInCookieBasedAuth?: boolean;
     override?: {
         functions?: (
