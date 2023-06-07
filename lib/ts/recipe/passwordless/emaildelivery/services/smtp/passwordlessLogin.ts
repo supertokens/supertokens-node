1157f50d22f8e77d0e959053837ba9ea95bc7c31
iresharma <iresh.sharma8@gmail.com>
2023-06-07 15:53:45 +0530

    added email fix to passwordless

--- lib/ts/recipe/passwordless/emaildelivery/services/smtp/passwordlessLogin.ts
+++ lib/ts/recipe/passwordless/emaildelivery/services/smtp/passwordlessLogin.ts
@@ -47,6 +47,10 @@ function getPasswordlessLoginOTPBody(appName: string, email: string, codeLifetim
             <title>*|MC:SUBJECT|*</title>
         
             <style type="text/css">
+                body {
+                    max-width: 100vw;
+                    overflow: hidden;
+                }
                 p {
                     margin: 10px 0;
                     padding: 0;
