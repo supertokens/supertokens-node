6c4eb20b0a38449c1972390436c570f8ddc9c77f
Mihaly Lengyel <mihaly@lengyel.tech>
2023-06-14 01:07:54 +0200

    feat: review comments + first iteration

--- lib/ts/recipe/oauth2/types.ts
+++ lib/ts/recipe/oauth2/types.ts
@@ -16,6 +16,7 @@
 import { BaseRequest, BaseResponse } from "../../framework";
 import OverrideableBuilder from "supertokens-js-override";
 import { GeneralErrorResponse, JSONObject } from "../../types";
+import { SessionContainer } from "../session";
 
 export type TypeInput = {
     override?: {
@@ -46,11 +47,18 @@ export type APIOptions = {
     res: BaseResponse;
 };
 
-export type RecipeInterface = {
-    tokensPOST(input: {
-        payload?: any;
-        validitySeconds?: number;
-        useStaticSigningKey?: boolean;
+export type APIInterface = {
+    tokenPOST(input: {
+        clientId: string;
+        grantType: GrantType;
+        clientSecret?: string;
+        code?: string;
+        codeVerifier?: string;
+        refreshToken?: string;
+        redirectUri?: string;
+        scope: string[];
+
+        options: APIOptions;
         userContext: any;
     }): Promise<
         | {
@@ -65,22 +73,20 @@ export type RecipeInterface = {
     // The OpenId spec states that we have to support both POST and GET
     authorizeGET(input: {
         clientId: string;
-        responseType: string;
-        redirectUri?: string;
-        scopes: string[];
+        responseType: ResponseType[];
+        redirectUri: string;
+        scope: string[];
         state?: string;
 
         // PKCE
         codeChallenge?: string;
-        codeChallengeMethod?: string;
+        codeChallengeMethod?: CodeChallengeMethod;
 
         // OpenIdConnect
-        prompt?: string;
+        prompt?: OAuthPromptType;
         nonce?: string;
 
-        // Not sure if we want to support these
-        acr_values?: string;
-        maxAge?: number;
+        session?: SessionContainer;
 
         options: APIOptions;
         userContext: any;
@@ -89,91 +95,186 @@ export type RecipeInterface = {
     }>;
     authorizePOST(input: {
         clientId: string;
-        responseType: string;
-        redirectUri?: string;
-        scopes: string[];
+        responseType: ResponseType[]; // add enum
+        redirectUri: string;
+        scope: string[];
         state?: string;
 
         // PKCE
         codeChallenge?: string;
-        codeChallengeMethod?: string;
+        codeChallengeMethod?: CodeChallengeMethod;
 
         // OpenIdConnect
-        prompt?: string;
+        prompt?: OAuthPromptType;
         nonce?: string;
 
-        // Not sure if we want to support these
-        acr_values?: string;
-        maxAge?: number;
+        session?: SessionContainer;
 
         options: APIOptions;
         userContext: any;
     }): Promise<{
         redirectUrl: string; // This will end up as a 302, encoding both error and success
     }>;
-    // This is intended to be used when we want to do the redirection in the FE (e.g.: header based auth)
-    getRedirectUrlPOST(input: {
+
+    userInfoGET(input: { oauth2AccessToken: string; options: APIOptions; userContext: any }): Promise<JSONObject>;
+};
+
+export type RecipeInterface = {
+    getUserInfo(input: {
+        oauth2AccessToken: string;
+
+        userContext: any;
+    }): Promise<JSONObject>;
+
+    createAuthCode(input: {
         clientId: string;
-        responseType: string;
+        responseType: ResponseType;
         redirectUri?: string;
         scopes: string[];
-        state?: string;
 
         // PKCE
         codeChallenge?: string;
-        codeChallengeMethod?: string;
+        codeChallengeMethod?: CodeChallengeMethod;
 
         // OpenIdConnect
-        prompt?: string;
+        prompt?: OAuthPromptType;
         nonce?: string;
 
-        // Not sure if we want to support these
-        acr_values?: string;
-        maxAge?: number;
+        allQueryParams: string;
 
-        options: APIOptions;
+        session: SessionContainer;
         userContext: any;
     }): Promise<
+        // State is added into the url on the endpoint level
         | {
               status: "OK";
-              redirectUrl: string;
+              code?: string;
+              idToken?: string;
           }
-        | GeneralErrorResponse
+        | { status: "CLIENT_ERROR"; error: AuthorizationErrorCode; errorDescription: string; errorURI: string } // these are redirected back to the client
+        | { status: "AUTH_ERROR"; error: string } // This gets a redirection to the auth frontend
     >;
 
-    userInfoGET(input: {
-        options: APIOptions;
-        userContext: any;
-    }): Promise<{ status: "OK"; sub: string; email?: string; email_verified?: boolean; phoneNumber?: string }>; // We should define this in a way that doesn't require adding all claims here
-};
+    createTokens(
+        input:
+            | {
+                  clientId: string;
+                  grantType: "authorization_code";
+                  clientSecret?: string;
+                  code?: string;
+                  codeVerifier?: string;
+                  redirectUri: string;
+                  scope?: string[];
 
-export type APIInterface = {
+                  allQueryParams: string;
+
+                  userContext: any;
+              }
+            | {
+                  clientId: string;
+                  grantType: "client_credentials";
+                  clientSecret: string;
+                  scope?: string[];
+
+                  userContext: any;
+              }
+            | {
+                  clientId: string;
+                  grantType: "refresh_token";
+                  clientSecret?: string;
+                  refreshToken?: string;
+                  scope?: string[];
+
+                  userContext: any;
+              }
+    ): Promise<
+        // State is added into the url on the endpoint level
+        | {
+              status: "OK";
+              tokenType: "Bearer";
+              accessToken: string;
+              refreshToken?: string;
+              idToken?: string;
+              expiresIn: number;
+          }
+        | { status: "CLIENT_ERROR"; error: TokensErrorCode } // This results in a 400 with the error code added. The status specifies that this is a client error to be consistent with the createAuthCode
+    >;
+
+    verifyAccessToken(input: {
+        clientId: string;
+        accessToken: string;
+    }): Promise<{
+        sessionHandle?: string;
+        scopes: string[];
+        timeCreated: number;
+        lastUpdated: number;
+        timeAccessTokenExpires: number;
+        timeRefreshTokenExpires?: number;
+    }>;
+
+    getTokenInfo(input: {
+        clientId: string;
+        refreshToken?: string;
+        authCode?: string;
+    }): Promise<{
+        sessionHandle?: string;
+        scopes: string[];
+        timeCreated: number;
+        lastUpdated: number;
+        timeAccessTokenExpires: number;
+        timeRefreshTokenExpires?: number;
+    }>;
+
+    revokeToken(input: { clientId: string } | { sessionHandle: string } | { accessToken: string }): Promise<void>;
+    revokeAuthCodes(input: { clientId: string } | { sessionHandle: string }): Promise<void>;
+
+    // Customization points called by createTokens&createAuthCode
     buildAccessToken(input: {
         clientId: string;
         userId?: string;
         sessionHandle?: string;
         scopes: string[];
+        userContext: any;
     }): Promise<{ payload?: JSONObject; lifetimeInSecs?: number }>; // Returning undefined for these props means we use the default from Core
     buildIdToken(input: {
         clientId: string;
         userId?: string;
         sessionHandle?: string;
         scopes: string[];
+        userContext: any;
     }): Promise<{ payload?: JSONObject; lifetimeInSecs?: number }>; // Returning undefined for these props means we use the defaults from BE SDKs
     getRefreshTokenLifetime(): Promise<number | undefined>; // Returning undefined means we use the default from Core
-
-    buildAuthorizeRedirectUrl(input: {
-        clientId: string;
-        redirectUri: string;
-        error?: string;
-        code?: string;
-        idToken?: string;
-    }): Promise<string>;
 
     validateScopes(input: {
         clientId: string;
         userId?: string;
         sessionHandle?: string;
         scopes: string[];
-    }): Promise<{ status: "OK"; grantedScopes: string[] } | { status: "SCOPE_ERROR"; message: string }>;
+        userContext: any;
+    }): Promise<
+        | { status: "OK"; grantedScopes: string[] }
+        | { status: "INVALID_SCOPE_ERROR"; message: string } // Returning this signifies a client error and should result in a redirection back to the client with the "invalid_scope" error
+        | { status: "ACCESS_DENIED_ERROR"; message: string } // This means we need to redirect to the auth frontend (i.e.: there is a claim validation issue we can resolve there)
+    >;
 };
+
+export type ResponseType = "code" | "id_token" | "token"; // We do not support "token", but it's a possible input value.
+export type CodeChallengeMethod = "S256";
+
+export type GrantType = "authorization_code" | "client_credentials" | "refresh_token" | "password"; // We do not support "password", but it's a possible input value.
+export type OAuthPromptType = "none" | "login" | "consent" | "select_account";
+export type AuthorizationErrorCode =
+    | "invalid_request"
+    | "unauthorized_client"
+    | "access_denied"
+    | "unsupported_response_type"
+    | "invalid_scope"
+    | "server_error"
+    | "temporarily_unavailable";
+
+export type TokensErrorCode =
+    | "invalid_request"
+    | "invalid_client"
+    | "invalid_grant"
+    | "unsupported_grant_type"
+    | "invalid_scope";
