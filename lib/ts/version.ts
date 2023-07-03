b54ec4cc54bad0a10df172cd95c5f3be80b5e2e4
Mihaly Lengyel <mihaly@lengyel.tech>
2023-07-03 10:56:20 +0200

    chore: update version number

--- lib/ts/version.ts
+++ lib/ts/version.ts
@@ -12,7 +12,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-export const version = "14.1.4";
+export const version = "14.1.3";
 
 export const cdiSupported = ["2.21"];
 
