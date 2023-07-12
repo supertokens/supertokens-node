1f0f29bb32ff4ebdf27b143fd9625a9d69a95f5f
Sattvik Chakravarthy <sattvik@gmail.com>
2023-07-12 12:05:45 +0530

    fix: pr comments

--- lib/ts/recipe/thirdparty/recipeImplementation.ts
+++ lib/ts/recipe/thirdparty/recipeImplementation.ts
@@ -130,7 +130,6 @@ export default function getRecipeImplementation(querier: Querier, providers: Pro
             return {
                 status: "OK",
                 provider,
-                thirdPartyEnabled: tenantConfig.thirdParty.enabled,
             };
         },
     };
