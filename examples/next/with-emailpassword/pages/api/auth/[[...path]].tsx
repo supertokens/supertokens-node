4606d623a9e7e58f192b03d50465fca4eb93969c
Mihaly Lengyel <mihaly@lengyel.tech>
2023-07-17 12:24:21 +0200

    docs: convert next example to use app dir

--- examples/next/with-emailpassword/pages/api/auth/[[...path]].tsx
+++ examples/next/with-emailpassword/pages/api/auth/[[...path]].tsx
@@ -1,14 +1,16 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { superTokensNextWrapper } from "supertokens-node/nextjs";
-import supertokens from "supertokens-node";
 import { middleware } from "supertokens-node/framework/express";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Request, Response } from "express";
+import supertokens from "supertokens-node";
 import { backendConfig } from "../../../config/backendConfig";
 
 supertokens.init(backendConfig());
 
-export default async function superTokens(req, res) {
+export default async function superTokens(req: NextApiRequest & Request, res: NextApiResponse & Response) {
     await superTokensNextWrapper(
         async (next) => {
+            // This is needed for production deployments with Vercel
             res.setHeader("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
             await middleware()(req, res, next);
         },
