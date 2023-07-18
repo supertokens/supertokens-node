9b939d1cbd774e77cd7714e7acb0b148225abe14
iresharma <iresh.sharma8@gmail.com>
2023-07-18 21:16:09 +0530

    fix express change

--- examples/express/with-emailpassword/frontend/src/config.tsx
+++ examples/express/with-emailpassword/frontend/src/config.tsx
@@ -4,7 +4,6 @@ import Session from "supertokens-auth-react/recipe/session";
 
 export function getApiDomain() {
     const apiPort = process.env.REACT_APP_API_PORT || 3001;
-    // const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
     const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
     return apiUrl;
 }
