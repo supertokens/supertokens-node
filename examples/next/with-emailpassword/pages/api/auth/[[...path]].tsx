74947ece407779a278f61e28f6b1da188460cd99
Mihaly Lengyel <mihaly@lengyel.tech>
2023-08-07 15:26:58 +0200

    docs(example): removed unnecessary check + added useShadowDom false

--- examples/next/with-emailpassword/pages/api/auth/[[...path]].tsx
+++ examples/next/with-emailpassword/pages/api/auth/[[...path]].tsx
@@ -10,11 +10,10 @@ export default async function superTokens(req: NextApiRequest, res: NextApiRespo
     await superTokensNextWrapper(
         async (next) => {
             // This is needed for production deployments with Vercel
-            if (!response.hasHeader("Cache-Control")) {
-                res.setHeader("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
-            }
+            // It'll be overwritten by the middleware in some cases (the jwks.json endpoint)
+            res.setHeader("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
 
-            await middleware()(req, res, next);
+            await middleware()(req as any, res as any, next);
         },
         req,
         res
