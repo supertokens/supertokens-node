efaebedd9fed051f55b8a1b45fc4efbcf3804999
Nemi Shah <nemishah1212@gmail.com>
2023-08-29 19:59:49 +0530

    refactor: Add handling for when Saas returns 429 because of rate limiting (#679)
    * Add retry logic to querier
    
    * Modify add dev tag

--- lib/ts/version.ts
+++ lib/ts/version.ts
@@ -12,7 +12,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-export const version = "13.6.0";
+export const version = "13.6.1";
 
 export const cdiSupported = [
     "2.8",
