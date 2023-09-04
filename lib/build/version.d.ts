15bbdbb42d311e65079fd32dc909801d341018ec
Nemi Shah <nemishah1212@gmail.com>
2023-09-04 13:17:17 +0530

    Add retry logic to querier (#684)

--- lib/build/version.d.ts
+++ lib/build/version.d.ts
@@ -1,4 +1,4 @@
 // @ts-nocheck
-export declare const version = "11.3.0";
+export declare const version = "11.3.1";
 export declare const cdiSupported: string[];
 export declare const dashboardVersion = "0.1";
