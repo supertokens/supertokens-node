54b8469fc3a6ad64d555bcb193eeec19510cbbff
Mihaly Lengyel <mihaly@lengyel.tech>
2023-09-23 18:46:31 +0200

    ci: debug parallel test execution

--- .circleci/setupAndTestWithAuthReact.sh
+++ .circleci/setupAndTestWithAuthReact.sh
@@ -70,8 +70,7 @@ cd ../../../supertokens-auth-react/
 # flag will not be checked because Auth0 is used as a provider so that the Thirdparty tests can run reliably. 
 # In versions lower than 0.18 Github is used as the provider.
 
-export CIRCLE_NODE_TOTAL;
-export CIRCLE_NODE_INDEX;
+sed -i "s/Start mocha testing\"/Start mocha testing_mod\"; printenv/" test/startTestApp.sh
 SKIP_OAUTH=true npm run test-with-non-node
 if [[ $? -ne 0 ]]
 then
