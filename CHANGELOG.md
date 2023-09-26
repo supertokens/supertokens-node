d4f4a263da0aaf65d9c0f12f7122bf7b286e992b
Sattvik Chakravarthy <sattvik@supertokens.com>
2023-09-26 14:15:47 +0530

    fix: twitter provider (#694)
    * fix: twitter provider
    
    * fix: build
    
    * fix: changelog
    
    * fix: twitter update and version update
    
    * fix: update version
    
    ---------
    
    Co-authored-by: Rishabh Poddar <rishabh.poddar@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html)
 
-## [unreleased]
+## [16.1.0] - 2023-09-26
+
+-   Added `twitter` as a built-in thirdparty provider
 
 ## [16.0.0] - 2023-09-25
 
--- lib/build/recipe/thirdparty/providers/custom.d.ts
+++ lib/build/recipe/thirdparty/providers/custom.d.ts
@@ -1,4 +1,6 @@
 // @ts-nocheck
 import { TypeProvider, ProviderInput } from "../types";
+export declare const DEV_OAUTH_REDIRECT_URL = "https://supertokens.io/dev/oauth/redirect-to-app";
+export declare function isUsingDevelopmentClientId(client_id: string): boolean;
 export declare function getActualClientIdFromDevelopmentClientId(client_id: string): string;
 export default function NewProvider(input: ProviderInput): TypeProvider;
