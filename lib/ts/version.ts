da453b7fa91c9d4357111df621c2fd9bcba9176b
Sattvik Chakravarthy <sattvik@gmail.com>
2023-09-26 14:11:27 +0530

    fix: update version

--- lib/ts/version.ts
+++ lib/ts/version.ts
@@ -12,7 +12,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-export const version = "16.0.1";
+export const version = "16.1.0";
 
 export const cdiSupported = ["4.0"];
 
