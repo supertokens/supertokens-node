fbcf43422b33818baa14285a314d3e85940f341b
Mihaly Lengyel <mihaly@lengyel.tech>
2023-10-07 01:11:35 +0200

    feat: move handleCall from nextjs appdir example

--- lib/build/nextjs.d.ts
+++ lib/build/nextjs.d.ts
@@ -1,9 +1,27 @@
 // @ts-nocheck
+import { HTTPMethod } from "./types";
+declare type PartialNextRequest = {
+    method: HTTPMethod;
+    url: string;
+    headers: Headers;
+    formData: () => any;
+    json: () => any;
+    cookies: {
+        getAll: () => {
+            name: string;
+            value: string;
+        }[];
+    };
+};
 export default class NextJS {
     static superTokensNextWrapper<T>(
         middleware: (next: (middlewareError?: any) => void) => Promise<T>,
         request: any,
         response: any
     ): Promise<T>;
+    static getAppDirRequestHandler<T extends PartialNextRequest>(
+        NextResponse: typeof Response
+    ): Promise<(req: T) => Promise<Response>>;
 }
 export declare let superTokensNextWrapper: typeof NextJS.superTokensNextWrapper;
+export {};
