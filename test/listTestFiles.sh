978b4a2d547f737152788f0336585274e9d54154
Mihaly Lengyel <mihaly@lengyel.tech>
2023-10-11 12:03:10 +0200

    ci: make test runs into a matrix job

---
+++ test/listTestFiles.sh
@@ -0,0 +1,9 @@
+matrixEntries=""
+delim=""
+while IFS=  read -r -d $'\0'; do
+    matrixEntries+=$delim"{\"testPath\": \""$(echo "$REPLY" )\""}";       # or however you want to process each file
+    delim=", "
+done < <(find ./examples -name *.test.js -print0)
+
+matrixEntries="[$matrixEntries]"
+echo $matrixEntries
