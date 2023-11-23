1f97facfbc107e43b0fb2d6ae68876689d0c71ae
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2023-11-23 12:08:32 +0530

    bump version v16.6.0

--- lib/build/version.d.ts
+++ lib/build/version.d.ts
@@ -1,4 +1,4 @@
 // @ts-nocheck
-export declare const version = "16.5.2";
+export declare const version = "16.6.0";
 export declare const cdiSupported: string[];
 export declare const dashboardVersion = "0.9";
