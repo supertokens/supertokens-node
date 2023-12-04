e84f3d48d45e96b3fc9b876dda1df8fe5d19293e
Sattvik Chakravarthy <sattvik@gmail.com>
2023-12-04 17:02:57 +0530

    fix: cdi version

--- lib/ts/version.ts
+++ lib/ts/version.ts
@@ -14,7 +14,7 @@
  */
 export const version = "16.3.4";
 
-export const cdiSupported = ["4.1"];
+export const cdiSupported = ["5.0"];
 
 // Note: The actual script import for dashboard uses v{DASHBOARD_VERSION}
 export const dashboardVersion = "0.8";
