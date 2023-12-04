af38995ac50955f5fc29bd9409b786a32f8f1da5
Ankit Tiwari <ankucodes@gmail.com>
2023-12-04 10:06:59 +0530

    fix: Update aws example to use esm imports (#750)

--- examples/aws/with-emailpassword/test/helpers/lambda-layer.sh
+++ examples/aws/with-emailpassword/test/helpers/lambda-layer.sh
@@ -1,6 +1,6 @@
 mkdir lambda && cd lambda
 npm init -y
-npm i -s @middy/core@4.5.5 @middy/http-cors@4.5.5
+npm i -s @middy/core @middy/http-cors
 npm i --save git+ssh://git@github.com:supertokens/supertokens-node.git#$GITHUB_REF_NAME
 mkdir nodejs
 cp -r node_modules nodejs
