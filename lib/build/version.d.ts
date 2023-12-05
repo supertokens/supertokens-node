4321aa5d049d78e6009f7fec9ea06760869b6b37
krzysztofwitkowski <krzysztof.witkowski@sysconsgroup.com>
2023-12-05 22:17:32 +0100

    fix(build command): runned

--- lib/build/version.d.ts
+++ lib/build/version.d.ts
@@ -1,4 +1,4 @@
 // @ts-nocheck
-export declare const version = "16.6.3";
+export declare const version = "16.6.4";
 export declare const cdiSupported: string[];
 export declare const dashboardVersion = "0.9";
