a89dd58168db7d76ad51bda46ddaf9a5943f1890
Sattvik Chakravarthy <sattvik@gmail.com>
2023-12-19 12:05:25 +0530

    fix: revert verifyEmailForRecipeUserIfLinkedAccountsAreVerified

--- lib/build/recipe/accountlinking/utils.d.ts
+++ lib/build/recipe/accountlinking/utils.d.ts
@@ -1,11 +1,4 @@
 // @ts-nocheck
-import RecipeUserId from "../../recipeUserId";
-import type { NormalisedAppinfo, UserContext } from "../../types";
-import { User } from "../../user";
+import type { NormalisedAppinfo } from "../../types";
 import type { TypeInput, TypeNormalisedInput } from "./types";
 export declare function validateAndNormaliseUserInput(_: NormalisedAppinfo, config?: TypeInput): TypeNormalisedInput;
-export declare function verifyEmailForRecipeUserIfLinkedAccountsAreVerified(input: {
-    user: User;
-    recipeUserId: RecipeUserId;
-    userContext: UserContext;
-}): Promise<void>;
