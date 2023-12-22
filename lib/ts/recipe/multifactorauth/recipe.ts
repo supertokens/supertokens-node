cba3eb185a6ac03dcbb1e34d60e653af342dd190
Sattvik Chakravarthy <sattvik@gmail.com>
2023-12-22 12:27:03 +0530

    fix: constant name

--- lib/ts/recipe/multifactorauth/recipe.ts
+++ lib/ts/recipe/multifactorauth/recipe.ts
@@ -21,7 +21,7 @@ import STError from "../../error";
 import { APIHandled, HTTPMethod, NormalisedAppinfo, RecipeListFunction, UserContext } from "../../types";
 import RecipeImplementation from "./recipeImplementation";
 import APIImplementation from "./api/implementation";
-import { UPDATE_SESSION_AND_GET_MFA_INFO } from "./constants";
+import { UPDATE_SESSION_AND_FETCH_MFA_INFO } from "./constants";
 import { MultiFactorAuthClaim } from "./multiFactorAuthClaim";
 import {
     APIInterface,
@@ -133,8 +133,8 @@ export default class Recipe extends RecipeModule {
         return [
             {
                 method: "put",
-                pathWithoutApiBasePath: new NormalisedURLPath(UPDATE_SESSION_AND_GET_MFA_INFO),
-                id: UPDATE_SESSION_AND_GET_MFA_INFO,
+                pathWithoutApiBasePath: new NormalisedURLPath(UPDATE_SESSION_AND_FETCH_MFA_INFO),
+                id: UPDATE_SESSION_AND_FETCH_MFA_INFO,
                 disabled: this.apiImpl.updateSessionAndFetchMfaInfoPUT === undefined,
             },
         ];
@@ -158,7 +158,7 @@ export default class Recipe extends RecipeModule {
             req,
             res,
         };
-        if (id === UPDATE_SESSION_AND_GET_MFA_INFO) {
+        if (id === UPDATE_SESSION_AND_FETCH_MFA_INFO) {
             return await mfaInfoAPI(this.apiImpl, options, userContext);
         }
         throw new Error("should never come here");
