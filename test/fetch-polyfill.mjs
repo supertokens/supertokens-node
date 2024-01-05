a5856842b6403107666290aa16ada964275c7112
Ankit Tiwari <ankucodes@gmail.com>
2024-01-05 18:26:37 +0530

    Upgrade node-fetch to v3

--- test/fetch-polyfill.mjs
+++ test/fetch-polyfill.mjs
@@ -3,11 +3,11 @@
 // https://github.com/nock/nock/issues/2397
 // https://github.com/nock/nock/issues/2183
 
-const fetch = require("node-fetch");
+import fetch, { Headers, Request, Response } from "node-fetch";
 
 if (!globalThis.fetch) {
     globalThis.fetch = fetch;
-    globalThis.Headers = fetch.Headers;
-    globalThis.Request = fetch.Request;
-    globalThis.Response = fetch.Response;
+    globalThis.Headers = Headers;
+    globalThis.Request = Request;
+    globalThis.Response = Response;
 }
