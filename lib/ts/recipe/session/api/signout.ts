43465f3f81797762cf76fb8520567d693a8c6caf
Sattvik Chakravarthy <sattvik@gmail.com>
2024-01-16 13:43:34 +0530

    fix: session required in signout

--- lib/ts/recipe/session/api/signout.ts
+++ lib/ts/recipe/session/api/signout.ts
@@ -35,7 +35,7 @@ export default async function signOutAPI(
         config: options.config,
         recipeInterfaceImpl: options.recipeImplementation,
         options: {
-            sessionRequired: false,
+            sessionRequired: true,
             overrideGlobalClaimValidators: () => [],
         },
         userContext,
