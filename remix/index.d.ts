539e120d10e77916b1e9c8622a3ba218ec702397
Mark Wentowski <mawentowski@Marks-MacBook-Pro.local>
2024-03-08 16:11:39 -0600

    feat: added new remix folder

---
+++ remix/index.d.ts
@@ -0,0 +1,11 @@
+export * from "../lib/build/nextjs";
+/**
+ * 'export *' does not re-export a default.
+ * import NextJS from "supertokens-node/nextjs";
+ * the above import statement won't be possible unless either
+ * - user add "esModuleInterop": true in their tsconfig.json file
+ * - we do the following change:
+ */
+
+import * as _default from "../lib/build/remix";
+export default _default;
