120c3dcccc5c258ec030f774cb5f6009385d5283
Mihály Lengyel <mihaly@lengyel.tech>
2024-03-18 16:05:51 +0100

    test: do not use try supertokens com in tests (#804)
    * test: remove usage of try.supertokens.com from tests
    
    * test: update integration server using Querier

--- test/frontendIntegration/index.js
+++ test/frontendIntegration/index.js
@@ -282,7 +282,8 @@ app.post("/login-2.18", async (req, res) => {
             enableAntiCsrf: false,
             userDataInJWT: payload,
             userDataInDatabase: {},
-        }
+        },
+        {}
     );
     Querier.apiVersion = undefined;
 
