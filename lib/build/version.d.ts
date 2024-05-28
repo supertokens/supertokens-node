5b13690e31a39fe41f9be472ef2556b591d01f94
Sattvik Chakravarthy <sattvik@supertokens.com>
2024-05-28 13:28:32 +0530

    fix: thirdparty recursion and config reference issues (#847)

--- lib/build/version.d.ts
+++ lib/build/version.d.ts
@@ -1,4 +1,4 @@
 // @ts-nocheck
-export declare const version = "15.2.2";
+export declare const version = "15.2.3";
 export declare const cdiSupported: string[];
 export declare const dashboardVersion = "0.7";
