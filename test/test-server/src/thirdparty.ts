d5abd422b325574b53b4c99c846caf2b2a742f91
Sandro de Souza <sandesou@gmail.com>
2024-06-10 13:30:50 -0300

    clean up

--- test/test-server/src/thirdparty.ts
+++ test/test-server/src/thirdparty.ts
@@ -6,37 +6,35 @@ import { logger } from "./logger";
 const namespace = "com.supertokens:node-test-server:thirdparty";
 const { logDebugMessage } = logger(namespace);
 
-const router = Router()
-    //
-    .post("/manuallycreateorupdateuser", async (req, res, next) => {
-        try {
-            logDebugMessage("ThirdParty:manuallyCreateOrUpdateUser %j", req.body);
-            let session = req.body.session && (await handleSession(req.body.session));
-            const response = await ThirdParty.manuallyCreateOrUpdateUser(
-                req.body.tenantId || "public",
-                req.body.thirdPartyId,
-                req.body.thirdPartyUserId,
-                req.body.email,
-                req.body.isVerified,
-                session,
-                req.body.userContext
-            );
-            res.json({
-                ...response,
-                ...("user" in response
-                    ? {
-                          user: response.user.toJson(),
-                      }
-                    : {}),
-                ...("recipeUserId" in response
-                    ? {
-                          recipeUserId: response.recipeUserId.getAsString(),
-                      }
-                    : {}),
-            });
-        } catch (e) {
-            next(e);
-        }
-    });
+const router = Router().post("/manuallycreateorupdateuser", async (req, res, next) => {
+    try {
+        logDebugMessage("ThirdParty:manuallyCreateOrUpdateUser %j", req.body);
+        let session = req.body.session && (await handleSession(req.body.session));
+        const response = await ThirdParty.manuallyCreateOrUpdateUser(
+            req.body.tenantId || "public",
+            req.body.thirdPartyId,
+            req.body.thirdPartyUserId,
+            req.body.email,
+            req.body.isVerified,
+            session,
+            req.body.userContext
+        );
+        res.json({
+            ...response,
+            ...("user" in response
+                ? {
+                      user: response.user.toJson(),
+                  }
+                : {}),
+            ...("recipeUserId" in response
+                ? {
+                      recipeUserId: response.recipeUserId.getAsString(),
+                  }
+                : {}),
+        });
+    } catch (e) {
+        next(e);
+    }
+});
 
 export default router;
