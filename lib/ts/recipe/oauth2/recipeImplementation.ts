568a2f34b78d6fc44390282d792811ef27b3cf9f
Ankit Tiwari <ankucodes@gmail.com>
2024-06-21 15:42:17 +0530

    feat: Add recipe functions to get OAuth2Clients (#865)
    * feat: Add recipe functions to update/delete OAuth2Client
    
    * fix: PR changes
    
    * feat: Add recipe functions to get OAuth2Clients
    
    * fix: PR changes
    
    ---------
    
    Co-authored-by: Mihaly Lengyel <mihaly@lengyel.tech>

--- lib/ts/recipe/oauth2/recipeImplementation.ts
+++ lib/ts/recipe/oauth2/recipeImplementation.ts
@@ -26,6 +26,45 @@ export default function getRecipeInterface(
     _appInfo: NormalisedAppinfo
 ): RecipeInterface {
     return {
+        getOAuth2Clients: async function (input, userContext) {
+            let response = await querier.sendGetRequest(
+                new NormalisedURLPath(`/recipe/oauth2/admin/clients`),
+                {
+                    ...transformObjectKeys(input, "snake-case"),
+                    page_token: input.paginationToken,
+                },
+                userContext
+            );
+
+            if (response.status === "OK") {
+                // Pagination info is in the Link header, containing comma-separated links:
+                // "first", "next" (if applicable).
+                // Example: Link: </admin/clients?page_size=5&page_token=token1>; rel="first", </admin/clients?page_size=5&page_token=token2>; rel="next"
+
+                // We parse the nextPaginationToken from the Link header using RegExp
+                let nextPaginationToken: string | undefined;
+                const linkHeader = response.headers.get("link");
+
+                const nextLinkMatch = linkHeader.match(/<([^>]+)>;\s*rel="next"/);
+                if (nextLinkMatch) {
+                    const url = nextLinkMatch[1];
+                    const urlParams = new URLSearchParams(url.split("?")[1]);
+                    nextPaginationToken = urlParams.get("page_token") as string;
+                }
+
+                return {
+                    status: "OK",
+                    clients: response.data.map((client: any) => OAuth2Client.fromAPIResponse(client)),
+                    nextPaginationToken,
+                };
+            } else {
+                return {
+                    status: "ERROR",
+                    error: response.data.error,
+                    errorHint: response.data.errorHint,
+                };
+            }
+        },
         createOAuth2Client: async function (input, userContext) {
             let response = await querier.sendPostRequest(
                 new NormalisedURLPath(`/recipe/oauth2/admin/clients`),
