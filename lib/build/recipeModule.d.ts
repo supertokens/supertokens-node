0b39ad9825dd27c073bf33fca717456ec9bbe35f
Ankit Tiwari <ankucodes@gmail.com>
2024-07-26 10:14:49 +0000

    fix: Prefer exact api path match in the middleware (#892)

--- lib/build/recipeModule.d.ts
+++ lib/build/recipeModule.d.ts
@@ -17,6 +17,7 @@ export default abstract class RecipeModule {
         | {
               id: string;
               tenantId: string;
+              exactMatch: boolean;
           }
         | undefined
     >;
