66e3cdb6c0c279b519879cc0a5d1b10e80e2b9e9
Deepjyoti Barman <joey31real@gmail.com>
2024-09-03 13:32:18 +0530

    Remove polyfill functionality for buffer and process

--- lib/ts/polyfill.ts
+++
@@ -1,9 +0,0 @@
-// Check if 'Buffer' is available, if not, polyfill it
-if (typeof Buffer === "undefined") {
-    global.Buffer = require("buffer").Buffer;
-}
-
-// Check if 'process' is available, if not, polyfill it
-if (typeof process === "undefined") {
-    global.process = require("process");
-}
