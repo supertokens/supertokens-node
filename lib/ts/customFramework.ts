daa0cb6071d8bb7da94fe2fd3ec7effb19422863
Deepjyoti Barman <deepjyoti@supertokens.com>
2024-10-02 10:42:01 +0530

    Add some more changes for the custom framework re-usability

--- lib/ts/customFramework.ts
+++ lib/ts/customFramework.ts
@@ -4,7 +4,13 @@
  * frameworks if they are not directly supported.
  */
 
-import { PreParsedRequest } from "./framework/custom";
+import { serialize } from "cookie";
+import { CollectingResponse, errorHandler, middleware, PreParsedRequest } from "./framework/custom";
+import Session, { SessionContainer, VerifySessionOptions } from "./recipe/session";
+import SessionRecipe from "./recipe/session/recipe";
+import { availableTokenTransferMethods } from "./recipe/session/constants";
+import { getToken } from "./recipe/session/cookieAndHeaders";
+import { parseJWTWithoutSignatureVerification } from "./recipe/session/jwt";
 
 // Define supported types for HTTPMethod
 export type HTTPMethod = "post" | "get" | "delete" | "put" | "options" | "trace";
@@ -59,4 +65,228 @@ function getQueryFromRequest(request: Request): Record<string, string> {
         query[key] = value;
     });
     return query;
+}
+
+function getAccessToken(request: Request): string | undefined {
+    return getCookieFromRequest(request)["sAccessToken"];
+}
+
+function getPublicKey(header: JwtHeader, callback: SigningKeyCallback) {
+    client.getSigningKey(header.kid, (err, key) => {
+        if (err) {
+            callback(err);
+        } else {
+            const signingKey = key?.getPublicKey();
+            callback(null, signingKey);
+        }
+    });
+}
+
+async function verifyToken(token: string): Promise<JwtPayload> {
+    return new Promise((resolve, reject) => {
+        JsonWebToken.verify(token, getPublicKey, {}, (err, decoded) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(decoded as JwtPayload);
+            }
+        });
+    });
+}
+
+export function handleAuthAPIRequest(CustomResponse: typeof Response) {
+    /**
+     * Util function to handle all calls by intercepting them, calling
+     * Supertokens middleware and then accordingly returning.
+     */
+    const stMiddleware = middleware<Request>((req) => {
+        return createPreParsedRequest(req);
+    });
+
+    return async function handleCall(req: Request) {
+        const baseResponse = new CollectingResponse();
+
+        const { handled, error } = await stMiddleware(req, baseResponse);
+
+        if (error) {
+            throw error;
+        }
+        if (!handled) {
+            return new CustomResponse("Not found", { status: 404 });
+        }
+
+        for (const respCookie of baseResponse.cookies) {
+            baseResponse.headers.append(
+                "Set-Cookie",
+                serialize(respCookie.key, respCookie.value, {
+                    domain: respCookie.domain,
+                    expires: new Date(respCookie.expires),
+                    httpOnly: respCookie.httpOnly,
+                    path: respCookie.path,
+                    sameSite: respCookie.sameSite,
+                    secure: respCookie.secure,
+                })
+            );
+        }
+
+        return new CustomResponse(baseResponse.body, {
+            headers: baseResponse.headers,
+            status: baseResponse.statusCode,
+        });
+    };
+}
+
+
+async function getSessionDetails(
+    preParsedRequest: PreParsedRequest,
+    options?: VerifySessionOptions,
+    userContext?: Record<string, unknown>
+): Promise<{
+    session: SessionContainer | undefined;
+    hasToken: boolean;
+    hasInvalidClaims: boolean;
+    baseResponse: CollectingResponse;
+    RemixResponse?: Response;
+}> {
+    const baseResponse = new CollectingResponse();
+    // Possible interop issue.
+    const recipe = (SessionRecipe as any).default.instance;
+    const tokenTransferMethod = recipe.config.getTokenTransferMethod({
+        req: preParsedRequest,
+        forCreateNewSession: false,
+        userContext,
+    });
+    const transferMethods = tokenTransferMethod === "any" ? availableTokenTransferMethods : [tokenTransferMethod];
+    const hasToken = transferMethods.some((transferMethod) => {
+        const token = getToken(preParsedRequest, "access", transferMethod);
+        if (!token) {
+            return false;
+        }
+        try {
+            parseJWTWithoutSignatureVerification(token);
+            return true;
+        } catch {
+            return false;
+        }
+    });
+
+    try {
+        const session = await Session.getSession(preParsedRequest, baseResponse, options, userContext);
+        return {
+            session,
+            hasInvalidClaims: false,
+            hasToken,
+            baseResponse,
+        };
+    } catch (err) {
+        if (Session.Error.isErrorFromSuperTokens(err)) {
+            return {
+                hasToken,
+                hasInvalidClaims: err.type === Session.Error.INVALID_CLAIMS,
+                session: undefined,
+                baseResponse,
+                RemixResponse: new Response("Authentication required", {
+                    status: err.type === Session.Error.INVALID_CLAIMS ? 403 : 401,
+                }),
+            };
+        } else {
+            throw err;
+        }
+    }
+}
+
+
+/**
+ * A helper function to retrieve session details on the server side.
+ *
+ * NOTE: This function does not use the getSession function from the supertokens-node SDK
+ * because getSession can update the access token. These updated tokens would not be
+ * propagated to the client side, as request interceptors do not run on the server side.
+ */
+export async function getSessionForSSR(remixRequest: Request): Promise<{
+    accessTokenPayload: JwtPayload | undefined;
+    hasToken: boolean;
+    error: Error | undefined;
+}> {
+    const accessToken = getAccessToken(remixRequest);
+    const hasToken = !!accessToken;
+    try {
+        if (accessToken) {
+            const decoded = await verifyToken(accessToken);
+            return { accessTokenPayload: decoded, hasToken, error: undefined };
+        }
+        return { accessTokenPayload: undefined, hasToken, error: undefined };
+    } catch (error) {
+        if (error instanceof JsonWebToken.TokenExpiredError) {
+            return { accessTokenPayload: undefined, hasToken, error: undefined };
+        }
+        return { accessTokenPayload: undefined, hasToken, error: error as Error };
+    }
+}
+
+export async function withSession(
+    remixRequest: Request,
+    handler: (error: Error | undefined, session: SessionContainer | undefined) => Promise<Response>,
+    options?: VerifySessionOptions,
+    userContext?: Record<string, any>
+): Promise<Response> {
+    try {
+        const baseRequest = createPreParsedRequest(remixRequest);
+        const { session, RemixResponse, baseResponse } = await getSessionDetails(baseRequest, options, userContext);
+
+        if (RemixResponse !== undefined) {
+            return RemixResponse;
+        }
+
+        let userResponse: Response;
+
+        try {
+            userResponse = await handler(undefined, session);
+        } catch (err) {
+            await errorHandler()(err, baseRequest, baseResponse, (errorHandlerError: Error) => {
+                if (errorHandlerError) {
+                    throw errorHandlerError;
+                }
+            });
+
+            // The headers in the userResponse are set twice from baseResponse, but the resulting response contains unique headers.
+            userResponse = new Response(baseResponse.body, {
+                status: baseResponse.statusCode,
+                headers: baseResponse.headers,
+            });
+        }
+
+        let didAddCookies = false;
+        let didAddHeaders = false;
+
+        for (const respCookie of baseResponse.cookies) {
+            didAddCookies = true;
+            userResponse.headers.append(
+                "Set-Cookie",
+                serialize(respCookie.key, respCookie.value, {
+                    domain: respCookie.domain,
+                    expires: new Date(respCookie.expires),
+                    httpOnly: respCookie.httpOnly,
+                    path: respCookie.path,
+                    sameSite: respCookie.sameSite,
+                    secure: respCookie.secure,
+                })
+            );
+        }
+
+        baseResponse.headers.forEach((value: string, key: string) => {
+            didAddHeaders = true;
+            userResponse.headers.set(key, value);
+        });
+        if (didAddCookies || didAddHeaders) {
+            if (!userResponse.headers.has("Cache-Control")) {
+                // This is needed for production deployments with Vercel
+                userResponse.headers.set("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
+            }
+        }
+
+        return userResponse;
+    } catch (error) {
+        return await handler(error as Error, undefined);
+    }
 }
