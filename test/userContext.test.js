supertokens/supertokens-node

commit 7c9e78ab3e5481e1acdfc0e07cc2f89aa1ebb6ab

refactor(types): Minor Type Improvements for the `ThirdParty` Recipe (#946)
* refactor(types): Make `userContext` in `TypeProvider` methods optional

It is safe to pass `undefined` for `userContext` in all of these
cases. Therefore, to improve the DX for developers not leveraging
`UserContext` for these methods, the argument should be optional.

* refactor(types): Clarify `redirectURIQueryParams` Type

This argument expects an object of strings. This should be
clarified to developers so that they don't mistakenly pass,
for instance, an instance of `URLSearchParams`.

* chore: Changelog updates

---------

Co-authored-by: Rishabh Poddar <rishabh.poddar@gmail.com>

lib/build/recipe/thirdparty/providers/custom.js
@@ -7,6 +7,7 @@ var __importDefault =
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getActualClientIdFromDevelopmentClientId = exports.isUsingDevelopmentClientId = exports.DEV_OAUTH_REDIRECT_URL = void 0;
 const thirdpartyUtils_1 = require("../../../thirdpartyUtils");
+const utils_1 = require("../../../utils");
 const pkce_challenge_1 = __importDefault(require("pkce-challenge"));
 const configUtils_1 = require("./configUtils");
 const jose_1 = require("jose");
@@ -284,7 +285,7 @@ function NewProvider(input) {
                     await impl.config.validateIdTokenPayload({
                         idTokenPayload: rawUserInfoFromProvider.fromIdTokenPayload,
                         clientConfig: impl.config,
-                        userContext,
+                        userContext: utils_1.getUserContext(userContext),
                     });
                 }
             }
@@ -292,7 +293,7 @@ function NewProvider(input) {
                 await impl.config.validateAccessToken({
                     accessToken: accessToken,
                     clientConfig: impl.config,
-                    userContext,
+                    userContext: utils_1.getUserContext(userContext),
                 });
             }
             if (accessToken && impl.config.userInfoEndpoint !== undefined) {

lib/build/recipe/thirdparty/types.d.ts
@@ -98,11 +98,11 @@ export declare type TypeProvider = {
     config: ProviderConfigForClientType;
     getConfigForClientType: (input: {
         clientType?: string;
-        userContext: UserContext;
+        userContext?: UserContext;
     }) => Promise<ProviderConfigForClientType>;
     getAuthorisationRedirectURL: (input: {
         redirectURIOnProviderDashboard: string;
-        userContext: UserContext;
+        userContext?: UserContext;
     }) => Promise<{
         urlWithQueryParams: string;
         pkceCodeVerifier?: string;
@@ -110,12 +110,12 @@ export declare type TypeProvider = {
     exchangeAuthCodeForOAuthTokens: (input: {
         redirectURIInfo: {
             redirectURIOnProviderDashboard: string;
-            redirectURIQueryParams: any;
+            redirectURIQueryParams: Record<string, string>;
             pkceCodeVerifier?: string;
         };
-        userContext: UserContext;
+        userContext?: UserContext;
     }) => Promise<any>;
-    getUserInfo: (input: { oAuthTokens: any; userContext: UserContext }) => Promise<UserInfo>;
+    getUserInfo: (input: { oAuthTokens: any; userContext?: UserContext }) => Promise<UserInfo>;
 };
 export declare type ProviderConfig = CommonProviderConfig & {
     clients?: ProviderClientConfig[];

lib/ts/recipe/thirdparty/providers/custom.ts
@@ -1,5 +1,6 @@
 import { TypeProvider, ProviderInput, UserInfo, ProviderConfigForClientType } from "../types";
 import { doGetRequest, doPostRequest, verifyIdTokenFromJWKSEndpointAndGetPayload } from "../../../thirdpartyUtils";
+import { getUserContext } from "../../../utils";
 import pkceChallenge from "pkce-challenge";
 import { getProviderConfigForClient } from "./configUtils";
 import { JWTVerifyGetKey, createRemoteJWKSet } from "jose";
@@ -312,7 +313,7 @@ export default function NewProvider(input: ProviderInput): TypeProvider {
                     await impl.config.validateIdTokenPayload({
                         idTokenPayload: rawUserInfoFromProvider.fromIdTokenPayload,
                         clientConfig: impl.config,
-                        userContext,
+                        userContext: getUserContext(userContext),
                     });
                 }
             }
@@ -321,7 +322,7 @@ export default function NewProvider(input: ProviderInput): TypeProvider {
                 await impl.config.validateAccessToken({
                     accessToken: accessToken,
                     clientConfig: impl.config,
-                    userContext,
+                    userContext: getUserContext(userContext),
                 });
             }
 

lib/ts/recipe/thirdparty/types.ts
@@ -98,21 +98,21 @@ export type TypeProvider = {
 
     getConfigForClientType: (input: {
         clientType?: string;
-        userContext: UserContext;
+        userContext?: UserContext;
     }) => Promise<ProviderConfigForClientType>;
     getAuthorisationRedirectURL: (input: {
         redirectURIOnProviderDashboard: string;
-        userContext: UserContext;
+        userContext?: UserContext;
     }) => Promise<{ urlWithQueryParams: string; pkceCodeVerifier?: string }>;
     exchangeAuthCodeForOAuthTokens: (input: {
         redirectURIInfo: {
             redirectURIOnProviderDashboard: string;
-            redirectURIQueryParams: any;
+            redirectURIQueryParams: Record<string, string>;
             pkceCodeVerifier?: string;
         };
-        userContext: UserContext;
+        userContext?: UserContext;
     }) => Promise<any>;
-    getUserInfo: (input: { oAuthTokens: any; userContext: UserContext }) => Promise<UserInfo>;
+    getUserInfo: (input: { oAuthTokens: any; userContext?: UserContext }) => Promise<UserInfo>;
 };
 
 export type ProviderConfig = CommonProviderConfig & {

commit c11fa71caacdaeae92397d9b73a63a5e4ce6833c

test: add exception for changes introduced in #946

test/userContext.test.js
@@ -556,6 +556,11 @@ describe(`userContext: ${printPath("[test/userContext.test.js]")}`, function () 
                     shouldNotContain: ["userContext: Record<String, any>", "userContext?: UserContext"],
                     canContain: [{ text: "userContext?: Record<String, any>", count: 1 }],
                 },
+                {
+                    fileName: "lib/ts/recipe/thirdparty/types.ts",
+                    shouldNotContain: ["userContext: Record<String, any>", "userContext?: Record<String, any>"],
+                    canContain: [{ text: "userContext?: UserContext", count: 4 }],
+                },
             ];
 
             let fileExceptionFound = false;