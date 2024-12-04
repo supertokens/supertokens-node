208ff6044633459a8ded3630d2f30d8eef21fc14
Deepjyoti Barman <deepjyoti@supertokens.com>
2024-12-04 09:53:05 +0530

    Use a simple report name for junit result reports

--- .circleci/setupAndTestBackendSDKWithFreeCore.sh
+++ .circleci/setupAndTestBackendSDKWithFreeCore.sh
@@ -115,9 +115,9 @@ export NODE_PORT=8081
 export INSTALL_PATH=../supertokens-root
 export multi="spec=- mocha-junit-reporter=/dev/null"
 
-mkdir test_report
+mkdir -p test-results
 TEST_FILES=$(circleci tests glob "test/**/*.test.js")
-echo "$TEST_FILES" | circleci tests run --command="xargs npx mocha mocha --reporter mocha-multi --node-option no-experimental-fetch -r test/fetch-polyfill.mjs --timeout 500000 --no-config" --verbose --split-by=timings
+echo "$TEST_FILES" | circleci tests run --command="xargs npx mocha mocha --reporter mocha-multi --reporter-options mocha-junit-reporter:output=test-results/junit.xml --node-option no-experimental-fetch -r test/fetch-polyfill.mjs --timeout 500000 --no-config" --verbose --split-by=timings
 
 # kill test-server
 kill $(lsof -t -i:$API_PORT)
